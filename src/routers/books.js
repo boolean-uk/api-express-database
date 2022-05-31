cb791c96275cf6b736d72819573698c20a7261c4
dearshrewdwit <dearshrewdwit@gmail.com>
2022-05-31 18:33:45 +0100

    Add simple error handling

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const db = require("../../db");
+const permittedTypes = ['Fiction', 'Non-Fiction']
 
 router.get('/', async (req, res) => {
-  const type = req.query.type
   const topic = req.query.topic
+  const type = req.query.type
+
+  if (type && !permittedTypes.includes(type)) {
+    res.status(400).json({error: `type: ${type} not valid. Accepted types are: ${permittedTypes}`})
+  }
+
   let sqlString = 'SELECT * FROM "books"'
   // as an example of interpolation
   if (type && topic) {
@@ -14,23 +20,36 @@ router.get('/', async (req, res) => {
   } else if (topic) {
     sqlString += ` WHERE topic = '${topic}';`
   }
-  const result = await db.query(sqlString)
-  res.json({books: result.rows})
+
+  try {
+    const result = await db.query(sqlString)
+    res.json({books: result.rows})
+  } catch (e) {
+    res.status(500).json({error: e.message})
+  }
 })
 
 router.get('/:id', async (req, res) => {
   const values = [req.params.id]
-  const result = await db.query(`SELECT * FROM "books" WHERE id = $1;`, values)
-  const book = result.rows[0]
-  res.json({book: book})
-  res.status(404).json({error: `no book with id: ${id}`})
+  try {
+    const result = await db.query(`SELECT * FROM "books" WHERE id = $1;`, values)
+    const book = result.rows[0]
+    if (!book) res.status(404).json({error: `no book with id: ${req.params.id}`})
+    res.json({book: book})
+  } catch (e) {
+    res.status(500).json({error: e.message})
+  }
 })
 
 router.post('/', async (req, res) => {
   const sqlString = `INSERT INTO "books" (title, type, author, topic, publicationDate, pages) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`
   const values = Object.values(req.body)
-  const result = await db.query(sqlString, values)
-  res.json({book: result.rows[0]})
+  try {
+    const result = await db.query(sqlString, values)
+    res.json({book: result.rows[0]})
+  } catch (e) {
+    res.status(500).json({error: e.message})
+  }
 })
 
 module.exports = router
--- src/routers/pets.js
+++ src/routers/pets.js
@@ -12,22 +12,35 @@ router.get('/', async (req, res) => {
     values.push(type)
   }
 
-  const result = await db.query(sqlString, values)
-  res.json({books: result.rows})
+  try {
+    const result = await db.query(sqlString, values)
+    res.json({books: result.rows})
+  } catch (e) {
+    res.status(500).json({error: e.message})
+  }
 })
 
 router.get('/:id', async (req, res) => {
   const values = [req.params.id]
-  const result = await db.query(`SELECT * FROM "pets" WHERE id = $1;`, values)
-  const book = result.rows[0]
-  res.json({book: book})
+  try {
+    const result = await db.query(`SELECT * FROM "pets" WHERE id = $1;`, values)
+    const book = result.rows[0]
+    if (!book) res.status(404).json({error: `no pet with id: ${req.params.id}`})
+    res.json({book: book})
+  } catch (e) {
+    res.status(500).json({error: e.message})
+  }
 })
 
 router.post('/', async (req, res) => {
   const sqlString = `INSERT INTO "pets" (name, age, type, breed, microchip) VALUES ($1, $2, $3, $4, $5) RETURNING *;`
   const values = Object.values(req.body)
-  const result = await db.query(sqlString, values)
-  res.json({book: result.rows[0]})
+  try {
+    const result = await db.query(sqlString, values)
+    res.json({book: result.rows[0]})
+  } catch (e) {
+    res.status(500).json({error: e.message})
+  }
 })
 
 module.exports = router
