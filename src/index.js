ce70b37605ae2b58f2df4a8befefb3a85376edbd
Hubert Lemczak <hubertlemczak@gmail.com>
2022-08-23 12:34:27 +0100

    get all books route with params

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-const app = require('./server.js')
-const db = require("../db");
-const port = 3030;
+const app = require('./server.js');
+const db = require('../db');
+const port = process.env.PORT || 3030;
 
 app.listen(port, () => {
-
   //Connect to the database
   db.connect(error => {
-
     //If there is an error connecting to the database,
     //log it out to the console
     if (error) {
-      console.error("[ERROR] Connection error: ", error.stack);
+      console.error('[ERROR] Connection error: ', error.stack);
     } else {
-      console.log("\n[DB] Connected...\n");
+      console.log('\n[DB] Connected...\n');
     }
   });
 
