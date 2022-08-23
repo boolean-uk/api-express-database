be2320995be007c9afdacf7fbbabd55ae755a20b
Hubert Lemczak <hubertlemczak@gmail.com>
2022-08-23 17:28:39 +0100

    fix: replaced nullish coalescing with logical OR

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const buildQuery = (base, query) => {
   const sqlParams = buildParams(query);
   const builtQuery = buildQueryString(base, query);
-
+  console.log(builtQuery, sqlParams);
   return [builtQuery, sqlParams];
 };
 
@@ -34,8 +34,8 @@ const buildQueryString = (base, query) => {
 };
 
 const buildPagination = (query, sqlQuery) => {
-  let page = query.page - 1 ?? 0;
-  let perPage = query.per_page ?? 20;
+  let page = query.page - 1 || 0;
+  let perPage = query.per_page || 20;
 
   if (page < 0) page = 0;
   if (perPage < 10) perPage = 10;
