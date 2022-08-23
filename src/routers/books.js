1d565ff8540b3ba0c52d93e0e2cd88a92c89b0b2
Hubert Lemczak <hubertlemczak@gmail.com>
2022-08-23 12:34:09 +0100

    get all books route with params

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,9 +1,29 @@
-const express = require('express')
-const router = express.Router()
-const db = require("../../db");
+const express = require('express');
+const router = express.Router();
+const db = require('../../db');
 
 router.get('/', async (req, res) => {
+  let sqlArgs = {
+    query: 'SELECT * FROM books',
+    params: [],
+  };
+  const { type, topic } = req.query;
 
-})
+  if (type && topic) {
+    sqlArgs.query += ' WHERE type = $1 AND topic = $2;';
+    sqlArgs.params.push(type, topic);
+  } else if (type) {
+    sqlArgs.query += ' WHERE type = $1;';
+    sqlArgs.params.push(type);
+  } else if (topic) {
+    sqlArgs.query += ' WHERE topic = $1;';
+    sqlArgs.params.push(type);
+  }
 
-module.exports = router
+  const dbRes = await db.query(sqlArgs.query, sqlArgs.params);
+  res.status(200).json({
+    books: dbRes.rows,
+  });
+});
+
+module.exports = router;
