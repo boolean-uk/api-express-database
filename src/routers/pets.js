ff295ea35a4d9c0f75f3f9e82530ba5c21348a95
Hubert Lemczak <hubertlemczak@gmail.com>
2022-08-23 15:08:49 +0100

    feat: pets router

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -4,60 +4,73 @@ const db = require('../../db');
 const { buildQuery } = require('../utils');
 
 router.get('/', async (req, res) => {
-  const base = 'SELECT * FROM books';
+  const base = 'SELECT * FROM pets';
   const [sqlQuery, sqlParams] = buildQuery(base, req.query);
 
   const dbRes = await db.query(sqlQuery, sqlParams);
 
   res.status(200).json({
-    books: dbRes.rows,
+    pets: dbRes.rows,
   });
 });
 
 router.get('/:id', async (req, res) => {
-  const bookId = req.params.id;
-  const sqlQuery = 'SELECT * FROM books WHERE id = $1';
+  const petId = req.params.id;
+  const sqlQuery = 'SELECT * FROM pets WHERE id = $1';
 
-  const dbRes = await db.query(sqlQuery, [bookId]);
+  const dbRes = await db.query(sqlQuery, [petId]);
 
   dbRes.rows.length > 0
     ? res.status(200).json({
-        book: dbRes.rows[0],
+        pet: dbRes.rows[0],
       })
     : res.sendStatus(404);
 });
 
 router.post('/', async (req, res) => {
-  const sqlQuery =
-    'INSERT INTO books(title, type, author, topic, publicationdate, pages) VALUES($1, $2, $3, $4, $5, $6) RETURNING *;';
+  const sqlQuery = `
+    INSERT INTO
+        pets(name, age, type, breed, microchip)
+    VALUES
+        ($1, $2, $3, $4, $5)
+    RETURNING *;`;
+
   const params = Object.values(req.body);
   const dbRes = await db.query(sqlQuery, params);
+
   res.status(201).json({
-    book: dbRes.rows[0],
+    pet: dbRes.rows[0],
   });
 });
 
 router.put('/:id', async (req, res) => {
-  const bookId = req.params.id;
+  const petId = req.params.id;
 
-  const sqlQuery =
-    'UPDATE books SET title = $1, type = $2, author = $3, topic = $4, publicationdate = $5, pages = $6 WHERE id = $7 RETURNING *;';
+  const sqlQuery = `
+    UPDATE
+        pets
+    SET
+        name = $1, age = $2, type = $3, breed = $4, microchip = $5
+    WHERE
+        id = $6
+    RETURNING *;`;
 
   const params = Object.values(req.body);
-  const dbRes = await db.query(sqlQuery, [...params, bookId]);
+  const dbRes = await db.query(sqlQuery, [...params, petId]);
+
   res.status(201).json({
-    book: dbRes.rows[0],
+    pet: dbRes.rows[0],
   });
 });
 
 router.delete('/:id', async (req, res) => {
-  const bookId = req.params.id;
+  const petId = req.params.id;
 
-  const sqlQuery = 'DELETE FROM books WHERE id = $1 RETURNING *;';
+  const sqlQuery = 'DELETE FROM pets WHERE id = $1 RETURNING *;';
+  const dbRes = await db.query(sqlQuery, [petId]);
 
-  const dbRes = await db.query(sqlQuery, [bookId]);
   res.status(201).json({
-    book: dbRes.rows[0],
+    pet: dbRes.rows[0],
   });
 });
 
