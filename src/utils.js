a9e5be1bac9407fe7e535475bb0133d7201aefd0
Hubert Lemczak <hubertlemczak@gmail.com>
2022-08-23 16:45:47 +0100

    refactor: buildQuery function reworked to support pagination

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,47 @@
 const buildQuery = (base, query) => {
-  let builtQuery = base;
-  const sqlParams = Object.values(query);
+  const sqlParams = buildParams(query);
+  const builtQuery = buildQueryString(base, query);
 
-  const params = Object.keys(query);
+  return [builtQuery, sqlParams];
+};
+
+const buildParams = query => {
+  const sqlParams = [];
+
+  for (const param in query) {
+    if (param === 'page' || param === 'per_page') {
+    } else sqlParams.push(query[param]);
+  }
+
+  return sqlParams;
+};
+
+const buildQueryString = (base, query) => {
+  let builtQuery = base;
+
+  const params = Object.keys(query)
+    .filter(p => p !== 'page')
+    .filter(p => p !== 'per_page');
 
   for (let i = 1; i <= params.length; i++) {
     if (i === 1) builtQuery += ` WHERE ${params[i - 1]} = $${i}`;
     else builtQuery += ` AND ${params[i - 1]} = $${i}`;
   }
 
-  return [builtQuery, sqlParams];
+  builtQuery = buildPagination(query, builtQuery);
+
+  return builtQuery;
+};
+
+const buildPagination = (query, sqlQuery) => {
+  let page = query.page - 1 ?? 0;
+  let perPage = query.per_page ?? 20;
+
+  if (page < 0) page = 0;
+  if (perPage < 10) perPage = 10;
+  if (perPage > 50) perPage = 50;
+
+  return (sqlQuery += ` OFFSET ${page * perPage} LIMIT ${perPage}`);
 };
 
 module.exports = { buildQuery };
