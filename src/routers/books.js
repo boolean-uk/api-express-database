587abfc6cf4c2d0033deb171f583f127c758fa73
wagnerAJS221 <wagneralex221@gmai.com>
2023-01-10 15:09:24 +0000

    get all books done

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const db = require("../../db");
+const db = require('../../db')
 
 router.get('/', async (req, res) => {
-
+  const result = await db.query('SELECT * FROM books')
+  res.json({ data: result.rows })
 })
 
 module.exports = router
