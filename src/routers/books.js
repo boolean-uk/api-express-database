4c104ad46adeaaf824a8416b0473d3a85ce84ec0
wagnerAJS221 <wagneralex221@gmai.com>
2023-01-10 16:04:00 +0000

    post request now working

--- src/routers/books.js
+++ src/routers/books.js
@@ -7,4 +7,15 @@ router.get('/', async (req, res) => {
   res.json({ data: result.rows })
 })
 
+router.post('/', async (req, res) => {
+  const { title, type, author, topic, publicationDate, pages } = req.body
+  const result = await db.query(
+    `INSERT INTO books (title, type, author, topic, "publicationDate", pages) 
+    VALUES ('${title}', '${type}','${author}', '${topic}','${publicationDate}', ${pages})
+    RETURNING *
+    `
+  )
+  res.json({ book: result.rows[0] })
+})
+
 module.exports = router
