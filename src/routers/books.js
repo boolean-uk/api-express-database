43153217c713811d5548ecc77f3059879f21a0d2
Fatima <fxtimaahmed14@gmail.com>
2023-01-10 15:52:34 +0000

    GET all boks done + working on POST new book

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,9 +1,19 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const db = require("../../db");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM books");
+  res.json({ books: result.rows });
+});
 
-})
+router.post("/", async (req, res) => {
+  const { title, type, author, topic, publicationDate, pages } = req.body;
+  const result = await db.query(`INSERT INTO books
+  ( title, type, author, topic, publicationDate, pages)
+  VALUES ('${title}', '${type}', '${author}','${topic}', '${publicationDate}', ${pages})
+  RETURNING * `);
+  res.json({ data: result.rows[0] });
+});
 
-module.exports = router
+module.exports = router;
