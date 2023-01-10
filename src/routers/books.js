b1e6ab124677fc438e2cbc52fdcbd8ef0af6f6b8
MattVwaves <jamesmattholt@gmail.com>
2023-01-10 16:59:47 +0000

    all basic req tests passing locally

--- src/routers/books.js
+++ src/routers/books.js
@@ -3,45 +3,45 @@ const router = express.Router();
 const db = require("../../db");
 
 router.get("/", async (req, res) => {
-  const result = await db.query("SELECT * FROM books");
-  res.json({ data: result.rows });
+  const books = await db.query("SELECT * FROM books");
+  res.json({ books: books.rows });
 });
 
 router.post("/", async (req, res) => {
   const { title, type, author, topic, publicationDate, pages } = req.body;
-  const result = await db.query(`
+  const book = await db.query(`
     INSERT INTO books (title, type, author, topic, "publicationDate", pages)
     VALUES ('${title}', '${type}', '${author}', '${topic}', '${publicationDate}', ${pages} )
     RETURNING *
     `);
-  res.json({ data: result.rows });
+  res.status(201).json( {book: book.rows[0]});
 });
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const result = await db.query(`SELECT * FROM books WHERE id = '${id}' `);
-  res.json({ data: result.rows });
+  const book = await db.query(`SELECT * FROM books WHERE id = '${id}' `);
+  res.json({ book: book.rows[0] });
 });
 
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { title, type, author, topic, publicationDate, pages } = req.body;
-  const result = await db.query(`
+  const book = await db.query(`
     UPDATE books 
     SET title = '${title}', type = '${type}', author = '${author}', topic = '${topic}', "publicationDate" = '${publicationDate}', pages = ${pages}
     WHERE id = ${id}
     RETURNING *
     `);
-  res.json({ data: result.rows });
+  res.status(201).json({ book: book.rows[0] });
 });
 
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const result = await db.query(`
+  const book = await db.query(`
     DELETE FROM books WHERE id = ${id}
     RETURNING *
     `);
-  res.json({ data: result.rows });
+  res.status(201).json({ book: book.rows[0] });
 });
 
 module.exports = router;
--- src/routers/pets.js
+++ src/routers/pets.js
@@ -3,36 +3,36 @@ const router = express.Router();
 const db = require("../../db");
 
 router.get("/", async (req, res) => {
-  const result = await db.query("SELECT * FROM pets");
-  res.json({ data: result.rows });
+  const pets = await db.query("SELECT * FROM pets");
+  res.json({ pets: pets.rows });
 });
 
 router.post("/", async (req, res) => {
   const { name, age, type, breed, microchip, pages } = req.body;
-  const result = await db.query(`
+  const pet = await db.query(`
     INSERT INTO pets (name, age, type, breed, microchip)
     VALUES ('${name}', ${age}, '${type}', '${breed}', ${microchip})
     RETURNING *
     `);
-  res.json({ data: result.rows });
+  res.status(201).json({ pet: pet.rows[0] });
 });
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const result = await db.query(`SELECT * FROM books WHERE id = '${id}' `);
-  res.json({ data: result.rows });
+  const pet = await db.query(`SELECT * FROM pets WHERE id = '${id}' `);
+  res.json({ pet: pet.rows[0] });
 });
 
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { name, age, type, breed, microchip } = req.body;
-  const result = await db.query(`
+  const pet = await db.query(`
     UPDATE pets 
     SET name = '${name}', age = ${age}, type = '${type}', breed = '${breed}', microchip = ${microchip}
     WHERE id = ${id}
     RETURNING *
     `);
-  res.json({ data: result.rows });
+  res.status(201).json({ pet: pet.rows[0] });
 });
 
 router.delete("/:id", async (req, res) => {
@@ -41,7 +41,7 @@ router.delete("/:id", async (req, res) => {
     DELETE FROM pets WHERE id = ${id}
     RETURNING *
     `);
-  res.json({ data: result.rows });
+  res.status(201).json({ pet: result.rows[0] });
 });
 
 module.exports = router;
