297af79d66adeecc9bf4c75fa1fb924880c9ef47
Fatima <fxtimaahmed14@gmail.com>
2023-01-10 16:06:39 +0000

    POST book done

--- src/routers/books.js
+++ src/routers/books.js
@@ -10,10 +10,10 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   const { title, type, author, topic, publicationDate, pages } = req.body;
   const result = await db.query(`INSERT INTO books
-  ( title, type, author, topic, publicationDate, pages)
+  ( title, type, author, topic, "publicationDate", pages)
   VALUES ('${title}', '${type}', '${author}','${topic}', '${publicationDate}', ${pages})
   RETURNING * `);
-  res.json({ data: result.rows[0] });
+  res.json(result.rows[0]);
 });
 
 module.exports = router;
