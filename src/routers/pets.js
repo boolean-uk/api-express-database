398bf25ef25c249296f8010fd286da0a1468e986
akibria12 <ahkibria@hotmail.co.uk>
2023-01-10 23:25:21 +0000

    pet update/delete/getbyID

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { as } = require("pg-promise");
 const router = express.Router();
 const db = require("../../db");
 
@@ -14,6 +15,28 @@ router.post("/", async (req, res) => {
     values('${name}','${age}','${type}','${breed}','${microchip}')
     returning *
 `);
-  res.json({ pets: createPet.rows[0] });
+  res.status(201).json({ pet: createPet.rows[0] });
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const getPetById = await db.query(`select * from pets where id=${id}`);
+  res.json({ pet: getPetById.rows });
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, age, type, breed, microchip } = req.body;
+  const updatedPet =
+    await db.query(`update pets set name = '${name}',age = '${age}',type='${type}',breed='${breed}',microchip='${microchip}' where id = '${id}'
+  returning *`);
+  res.status(201).json({ pet: updatedPet.rows[0] });
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletePet = await db.query(`delete from pets where id = '${id}'
+  returning *`);
+  res.status(201).json({ pets: deletePet.rows[0] });
 });
 module.exports = router;
