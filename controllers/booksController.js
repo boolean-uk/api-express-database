e1309b2d990d98e2bc5f1a7cddbd911bf4357999
wagnerAJS221 <wagneralex221@gmai.com>
2023-01-11 17:15:55 +0000

    error messages in there now

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,53 +1,84 @@
-const db = require("../db/index");
+const db = require('../db/index')
 
 const {
   getAll,
   create,
   byId,
   update,
-  bookRemove,
-} = require("../queries/bookQueries");
+  bookRemove
+} = require('../queries/bookQueries')
 
 const getAllBooks = async (req, res) => {
-  const { type, topic } = req.query;
+  const { type, topic, author } = req.query
 
-  const result = await getAll(type, topic);
+  const result = await getAll(type, topic, author)
 
-  res.json({ books: result });
-};
+  res.json({ books: result })
+}
 
 const createBook = async (req, res) => {
-  const { title, type, author, topic, publicationDate, pages } = req.body;
-  const values = [title, type, author, topic, publicationDate, pages];
-  const result = await create(values);
-  res.status(201).json({ book: result });
-};
+  const { title, type, author, topic, publicationDate, pages } = req.body
+  const values = [title, type, author, topic, publicationDate, pages]
+  const result = await create(values)
+  res.status(201).json({ book: result })
+}
 
 const getBookById = async (req, res) => {
-  const { id } = req.params;
-  const result = await byId(id);
-  res.json({ book: result });
-};
+  const { id } = req.params
+  try {
+    const result = await byId(id)
+    if (!result) {
+      res.status(404).json({
+        error: 'A book with the provided ID does not exist'
+      })
+    } else {
+      res.json({ book: result })
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
 
 const updateBook = async (req, res) => {
-  const { id } = req.params;
-  const { title, type, author, topic, publicationDate, pages } = req.body;
-  const values = [title, type, author, topic, publicationDate, pages, id];
-  const result = await update(values);
-
-  res.status(201).json({ book: result });
-};
+  const { id } = req.params
+  const { title, type, author, topic, publicationDate, pages } = req.body
+  const values = [title, type, author, topic, publicationDate, pages, id]
+  try {
+    const result = await update(values)
+    if (!result) {
+      res.status(404).json({
+        error: 'A book with the provided ID does not exist'
+      })
+    } else {
+      res.status(201).json({ book: result })
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
 
 const deleteBook = async (req, res) => {
-  const { id } = req.params;
-  const result = await bookRemove(id);
-  res.status(201).json({ book: result });
-};
+  const { id } = req.params
+
+  try {
+    const result = await bookRemove(id)
+    if (!result) {
+      res.status(404).json({
+        error:
+          'A book with the provided ID does not exist and connot be deleted! You obviously cant delete something that doesnt exist, bruh !'
+      })
+    } else {
+      res.status(201).json({ book: result })
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
 
 module.exports = {
   getAllBooks,
   createBook,
   getBookById,
   updateBook,
-  deleteBook,
-};
+  deleteBook
+}
--- queries/bookQueries.js
+++ queries/bookQueries.js
@@ -1,61 +1,65 @@
-const db = require("../db/index");
+const db = require('../db/index')
 
-const getAll = async (type, topic) => {
+const getAll = async (type, topic, author) => {
   if (type && topic) {
     const result = await db.query(
-      "SELECT * FROM books WHERE type = $1 AND topic = $2",
+      'SELECT * FROM books WHERE type = $1 AND topic = $2',
       [type, topic]
-    );
-    return result.rows;
+    )
+    return result.rows
   }
   if (type) {
-    const result = await db.query("SELECT * FROM books WHERE type = $1", [
-      type,
-    ]);
-    return result.rows;
+    const result = await db.query('SELECT * FROM books WHERE type = $1', [type])
+    return result.rows
   }
   if (topic) {
-    const result = await db.query("SELECT * FROM books WHERE topic = $1", [
-      topic,
-    ]);
-    return result.rows;
+    const result = await db.query('SELECT * FROM books WHERE topic = $1', [
+      topic
+    ])
+    return result.rows
   }
-  const result = await db.query("SELECT * FROM books");
-  return result.rows;
-};
+  if (author) {
+    const result = await db.query('SELECT * FROM books WHERE author = $1', [
+      author
+    ])
+    return result.rows
+  }
+  const result = await db.query('SELECT * FROM books')
+  return result.rows
+}
 
 const create = async (values) => {
   const result = await db.query(
     'INSERT INTO books (title, type, author, topic, "publicationDate", pages) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
     values
-  );
-  return result.rows[0];
-};
+  )
+  return result.rows[0]
+}
 
 const byId = async (id) => {
-  const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
-  return result.rows[0];
-};
+  const result = await db.query('SELECT * FROM books WHERE id = $1', [id])
+  return result.rows[0]
+}
 
 const update = async (values) => {
   const result = await db.query(
     'UPDATE books SET title = $1, type = $2, author = $3, topic = $4, "publicationDate" = $5, pages = $6 WHERE id = $7 RETURNING *',
     values
-  );
-  return result.rows[0];
-};
+  )
+  return result.rows[0]
+}
 
 const bookRemove = async (id) => {
-  const result = await db.query("DELETE FROM books WHERE id = $1 RETURNING *", [
-    id,
-  ]);
-  return result.rows[0];
-};
+  const result = await db.query('DELETE FROM books WHERE id = $1 RETURNING *', [
+    id
+  ])
+  return result.rows[0]
+}
 
 module.exports = {
   getAll,
   create,
   byId,
   update,
-  bookRemove,
-};
+  bookRemove
+}
