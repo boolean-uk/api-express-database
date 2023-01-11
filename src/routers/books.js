84ce348cf75c22b1c6eaed181caa4760c28fb1fa
akibria12 <ahkibria@hotmail.co.uk>
2023-01-11 00:47:57 +0000

    error fixed and all core test passed

--- src/routers/books.js
+++ src/routers/books.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const db = require("../../db");
 
 router.get("/", async (req, res) => {
-  const result = await db.query("SELECT * FROM books");
-  res.json({ data: result.rows });
+  const book = await db.query("SELECT * FROM books");
+  res.json({ books: book.rows });
 });
 
 router.post("/", async (req, res) => {
   const { title, type, author, topic, publicationDate, pages } = req.body;
-  const result =
+  const book =
     await db.query(`INSERT INTO books (title, type, author, topic, "publicationDate", pages)
     VALUES ('${title}', '${type}', '${author}', '${topic}', '${publicationDate}', '${pages}')    
     RETURNING *
     `);
-  res.json({ data: result.rows[0] });
+  res.status(201).json({ book: book.rows[0] });
 });
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const result = await db.query(`SELECT * FROM books WHERE id = ${id}`);
-  res.json({ data: result.rows });
+  const book = await db.query(`SELECT * FROM books WHERE id = '${id}'`);
+  res.json({ book: book.rows[0] });
 });
 
 router.put("/:id", async (req, res) => {
--- src/routers/pets.js
+++ src/routers/pets.js
@@ -4,39 +4,38 @@ const router = express.Router();
 const db = require("../../db");
 
 router.get("/", async (req, res) => {
-  const getAllPets = await db.query("select * from pets");
-  res.json({ pets: getAllPets.rows });
+  const pet = await db.query("select * from pets");
+  res.json({ pets: pet.rows });
 });
 
 router.post("/", async (req, res) => {
   const { name, age, type, breed, microchip } = req.body;
-  const createPet =
-    await db.query(`insert into pets (name,age,type,breed, microchip)
+  const pet = await db.query(`insert into pets (name,age,type,breed, microchip)
     values('${name}','${age}','${type}','${breed}','${microchip}')
     returning *
 `);
-  res.status(201).json({ pet: createPet.rows[0] });
+  res.status(201).json({ pet: pet.rows[0] });
 });
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const getPetById = await db.query(`select * from pets where id=${id}`);
-  res.json({ pet: getPetById.rows });
+  const pet = await db.query(`select * from pets where id=${id}`);
+  res.json({ pet: pet.rows[0] });
 });
 
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { name, age, type, breed, microchip } = req.body;
-  const updatedPet =
+  const pet =
     await db.query(`update pets set name = '${name}',age = '${age}',type='${type}',breed='${breed}',microchip='${microchip}' where id = '${id}'
   returning *`);
-  res.status(201).json({ pet: updatedPet.rows[0] });
+  res.status(201).json({ pet: pet.rows[0] });
 });
 
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const deletePet = await db.query(`delete from pets where id = '${id}'
-  returning *`);
-  res.status(201).json({ pets: deletePet.rows[0] });
+  const pet = await db.query(`DELETE FROM pets WHERE id = ${id}
+  RETURNING *`);
+  res.status(201).json({ pet: pet.rows[0] });
 });
 module.exports = router;
