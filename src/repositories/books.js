61ca51de6305c72e8b042f2d1f7d85bf34eee9e1
Haroun Anwari <harounanwari@hotmail.co.uk>
2023-01-11 17:49:38 +0000

    started pagination

--- src/repositories/books.js
+++ src/repositories/books.js
@@ -1,71 +1,85 @@
 const db = require("../../db");
 
 const getAllBooks = async (req, res) => {
-	return db.query(`SELECT * FROM books`);
+  return db.query(`SELECT * FROM books`);
 };
 
 const getAllBooksByTypeOrTopic = async (
-	type,
-	topic,
-	author,
-	page,
-	per_page
+  type,
+  topic,
+  author,
+  page,
+  per_page
 ) => {
-	let filters = "";
+  let filters = "";
+  let authorQuery = "";
+  let per_page_number = 20;
+  let pageNumber = 1;
+  // if statements for type/topic/author
+  if (author) {
+    authorQuery = "AND author = $1";
+  }
 
-	// if statements for type/topic/author
-	if (author) {
-		console.log("");
-	}
-	// use concat(?) at the end to add LIMIT/OFFSET based on page/per_page
-	// Check if page/per_page have been defined or if default values need to be assigned
-	// OFFSET will be per_page * page, LIMIT will be interpolation of per_page
+  if (per_page) {
+    per_page_number = `LIMIT $1`;
+  }
+  if (page) {
+    offsetNumber = page * per_page;
 
-	if (type && topic) {
-		filters = `WHERE type = $1 AND topic = $2`;
+    return (pageNumber = offsetNumber);
+  }
+  // use concat(?) at the end to add LIMIT/OFFSET based on page/per_page
+  // Check if page/per_page have been defined or if default values need to be assigned
+  // OFFSET will be per_page * page, LIMIT will be interpolation of per_page
 
-		return db.query(`SELECT * FROM books ${filters}`, [type, topic]);
-	} else if (type) {
-		filters = `WHERE type = $1`;
-		return db.query(`SELECT * FROM books ${filters}`, [type]);
-	} else if (topic) {
-		filters = `WHERE topic = $1`;
-		return db.query(`SELECT * FROM books ${filters}`, [topic]);
-	}
+  if (type && topic) {
+    filters = `WHERE type = $1 AND topic = $2`;
+    filters = filters.concat(` ${authorQuery}`);
+    filters = filters.concat(` ${pageNumber}`);
+    filters = filters.concat(` ${per_page_number}`);
+
+    return db.query(`SELECT * FROM books ${filters}`, [type, topic]);
+  } else if (type) {
+    filters = `WHERE type = $1`;
+    return db.query(`SELECT * FROM books ${filters}`, [type]);
+  } else if (topic) {
+    filters = `WHERE topic = $1`;
+    return db.query(`SELECT * FROM books ${filters}`, [topic]);
+  }
 };
 
 const getBookByID = async (id) => {
-	return db.query(`SELECT * FROM books WHERE id = $1`, [id]);
+  return db.query(`SELECT * FROM books WHERE id = $1`, [id]);
 };
 
 const updateBookByID = async (id, values) => {
-	const result = await db.query(
-		`UPDATE books SET title = $1, type = $2, author = $3, topic = $4, "publicationDate" = $5, pages = $6 WHERE id = $7 RETURNING *`,
-		[...values, id]
-	);
-	return result.rows[0];
+  const result = await db.query(
+    `UPDATE books SET title = $1, type = $2, author = $3, topic = $4, "publicationDate" = $5, pages = $6 WHERE id = $7 RETURNING *`,
+    [...values, id]
+  );
+  return result.rows[0];
 };
 
 const createBook = async (values) => {
-	const result = await db.query(
-		'INSERT INTO books(title, type, author,topic, "publicationDate", pages) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-		values
-	);
-	return result.rows[0];
+  const result = await db.query(
+    'INSERT INTO books(title, type, author,topic, "publicationDate", pages) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+    values
+  );
+  return result.rows[0];
 };
 
 const deleteBookByID = async (id) => {
-	const result = await db.query("DELETE from books WHERE id = $1 RETURNING *", [
-		id,
-	]);
-	return result.rows[0];
+  const result = await db.query("DELETE from books WHERE id = $1 RETURNING *", [
+    id,
+  ]);
+  return result.rows[0];
 };
 
 module.exports = {
-	getAllBooks,
-	getAllBooksByTypeOrTopic,
-	getBookByID,
-	createBook,
-	updateBookByID,
-	deleteBookByID,
+  getAllBooks,
+  getAllBooksByTypeOrTopic,
+  getBookByID,
+  createBook,
+  updateBookByID,
+  deleteBookByID,
 };
