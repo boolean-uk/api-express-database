cf48c9326cab51006b90076ab74894b7c5112a5f
wagnerAJS221 <wagneralex221@gmai.com>
2023-02-15 16:56:23 +0000

    all core tests passing

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,45 +1,46 @@
-const db = require("../db/index");
+const db = require('../db/index')
 const {
   getAll,
   create,
   byId,
   update,
-  petRemove,
-} = require("../queries/petQueries");
+  petRemove
+} = require('../queries/petQueries')
 
 const getAllPets = async (req, res) => {
-  const { type } = req.query;
+  const { type } = req.query
 
-  const result = await getAll(type);
-  res.json({ pets: result });
-};
+  const result = await getAll(type)
+  res.json({ pets: result })
+}
 
 const createPet = async (req, res) => {
-  const { name, age, type, breed, microchip } = req.body;
-  const values = [name, age, type, breed, microchip];
-  const result = await create(values);
-  res.status(201).json({ pet: result });
-};
+  const { name, age, type, breed, microchip } = req.body
+  const values = [name, age, type, breed, microchip]
+  const result = await create(values)
+  res.status(201).json({ pet: result })
+}
 
 const getPetById = async (req, res) => {
-  const id = req.params.id;
-  const result = await byId(id);
+  const id = req.params.id
+  const result = await byId(id)
 
-  res.json({ pet: result });
-};
+  res.json({ pet: result })
+}
 
 const updatePet = async (req, res) => {
-  const { id } = req.params;
-  const { name, age, type, breed, microchip } = req.body;
-  const values = [name, age, type, breed, microchip, id];
-  const result = await update(values);
-  res.status(201).json({ pets: result });
-};
+  const { id } = req.params
+  const { name, age, type, breed, microchip } = req.body
+  const values = [name, age, type, breed, microchip, id]
+  const result = await update(values)
+  console.log(res.body)
+  res.status(201).json({ pet: result })
+}
 
 const deletePet = async (req, res) => {
-  const { id } = req.params;
-  const result = await petRemove(id);
-  res.status(201).json({ pet: result });
-};
+  const { id } = req.params
+  const result = await petRemove(id)
+  res.status(201).json({ pet: result })
+}
 
-module.exports = { getAllPets, createPet, getPetById, updatePet, deletePet };
+module.exports = { getAllPets, createPet, getPetById, updatePet, deletePet }
--- queries/petQueries.js
+++ queries/petQueries.js
@@ -1,41 +1,41 @@
-const db = require("../db/index");
+const db = require('../db/index')
 
 const getAll = async (type) => {
   if (type) {
-    const result = await db.query("SELECT * FROM pets WHERE type = $1", [type]);
-    return result.rows;
+    const result = await db.query('SELECT * FROM pets WHERE type = $1', [type])
+    return result.rows
   }
-  const result = await db.query("SELECT * FROM pets");
-  return result.rows;
-};
+  const result = await db.query('SELECT * FROM pets')
+  return result.rows
+}
 
 const create = async (values) => {
   const result = await db.query(
-    "INSERT INTO pets (name, age, type , breed, microchip) VALUES ($1, $2, $3,$4,$5) RETURNING *",
+    'INSERT INTO pets (name, age, type , breed, microchip) VALUES ($1, $2, $3,$4,$5) RETURNING *',
     values
-  );
-  return result.rows[0];
-};
+  )
+  return result.rows[0]
+}
 
 const byId = async (id) => {
-  const result = await db.query("SELECT * FROM pets WHERE id = $1", [id]);
-  return result.rows[0];
-};
+  const result = await db.query('SELECT * FROM pets WHERE id = $1', [id])
+  return result.rows[0]
+}
 
 const update = async (values) => {
   const result = await db.query(
-    "UPDATE pets SET name = $1, age = $2, type = $3, breed = $4, microchip = $5 WHERE id = $6 RETURNING *",
+    'UPDATE pets SET name = $1, age = $2, type = $3, breed = $4, microchip = $5 WHERE id = $6 RETURNING *',
     values
-  );
+  )
 
-  return result.rows[0];
-};
+  return result.rows[0]
+}
 
 const petRemove = async (id) => {
-  const result = await db.query(" DELETE FROM pets WHERE id = $1 RETURNING *", [
-    id,
-  ]);
-  return result.rows[0];
-};
+  const result = await db.query(' DELETE FROM pets WHERE id = $1 RETURNING *', [
+    id
+  ])
+  return result.rows[0]
+}
 
-module.exports = { getAll, create, byId, update, petRemove };
+module.exports = { getAll, create, byId, update, petRemove }
