c6d77e3c8a5e2261df6b80345744ed586db7bc6f
benji <benjipictor@gmail.com>
2023-03-17 11:13:45 +0000

    setup done

--- src/routers/books.js
+++ src/routers/books.js
@@ -3,7 +3,19 @@ const router = express.Router()
 const db = require("../../db");
 
 router.get('/', async (req, res) => {
+    const type = req.query.type
 
+    let str = 'SELECT * FROM books'
+    let values = []
+    if (type) {
+        str += ' WHERE type = $1' // if using interpolation instead there is a possibility injecting SQL -> 'intentionally' drop table?
+        values = [type]
+    }
+    str += ';'
+
+    const data = await db.query(str, values)
+    const books = data.rows
+    res.json({ books })
 })
 
 module.exports = router
