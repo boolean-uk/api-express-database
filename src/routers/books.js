8ac3d50baf0e7f55fa36beab68fbd76a87512d90
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-03-17 10:55:04 +0000

    get all books

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,9 +1,29 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const db = require("../../db");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  const bookdata = await db.query("SELECT * FROM books;");
 
-})
+  res.json({ books: bookdata.rows });
+});
 
-module.exports = router
+// router.get("/", async (req, res) => {
+//   const topic = req.query.topic;
+//   const values = [];
+
+//   let str = "SELECT * FROM books";
+
+//   if (topic) {
+//     str += " WHERE topic = $1";
+//     values.push(topic);
+//   }
+//   str += ";";
+
+//   const bookdata = await db.query(str, values);
+//   const books = bookdata.rows;
+
+//   res.json({ books });
+// });
+
+module.exports = router;
