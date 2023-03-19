05ccdbd2fce093d97a8175f7e89add9f1673743e
Oliver West <oliversimon.west@gmail.com>
2023-03-19 22:49:35 +0000

    implemented pets router to controller to repo

--- src/controllers/pets.js
+++ src/controllers/pets.js
@@ -1,53 +1,37 @@
 const petsRepo = require('../repositories/pets');
-const db = require('../../db');
 
 const getAllPets = async (req, res) => {
-    let str = 'SELECT * FROM pets';
     let values = [];
-    str += ';';
-    const data = await db.query(str, values);
-    const pets = data.rows;
+    const pets = await petsRepo.getAllPets(values);
     res.json({ pets });
 };
 
 const getPetById = async (req, res) => {
     const id = req.params.id;
-    const str = 'SELECT * FROM pets WHERE id = $1;';
     const values = [id];
-    const data = await db.query(str, values);
-    const pet = data.rows[0];
+    const pet = await petsRepo.getPetById(values);
     res.json({ pet });
 };
 
 const createPet = async (req, res) => {
     const { name, age, type, breed, microchip } = req.body;
-    const str =
-        'INSERT INTO pets (name, age, type, breed, microchip) VALUES ($1, $2, $3, $4, $5) RETURNING *;';
     const values = [name, age, type, breed, microchip];
-    const data = await db.query(str, values);
-    const pet = data.rows[0];
-
+    const pet = await petsRepo.createPet(values);
     res.status(201).json({ pet });
 };
 
 const updatePetById = async (req, res) => {
     const id = req.params.id;
     const { name, age, type, breed, microchip } = req.body;
-
-    const str =
-        'UPDATE pets SET name = $1, age = $2, type = $3, breed = $4, microchip = $5 WHERE id = $6 RETURNING *;';
     const values = [name, age, type, breed, microchip, id];
-    const data = await db.query(str, values);
-    const pet = data.rows[0];
+    const pet = await petsRepo.updatePetById(values);
     res.status(201).json({ pet });
 };
 
 const deletePetById = async (req, res) => {
     const id = req.params.id;
-    const str = 'DELETE from pets WHERE id = $1 RETURNING *;';
     const values = [id];
-    const data = await db.query(str, values);
-    const pet = data.rows[0];
+    const pet = await petsRepo.deletePetById(values);
     res.status(201).json({ pet });
 };
 
--- src/repositories/pets.js
+++ src/repositories/pets.js
@@ -1,3 +1,47 @@
 const db = require('../../db');
 
-module.exports = {};
+const getAllPets = async (values) => {
+    let str = 'SELECT * FROM pets';
+    str += ';';
+    const data = await db.query(str, values);
+    const pets = data.rows;
+    return pets;
+};
+
+const getPetById = async (values) => {
+    const str = 'SELECT * FROM pets WHERE id = $1;';
+    const data = await db.query(str, values);
+    const pet = data.rows[0];
+    return pet;
+};
+
+const createPet = async (values) => {
+    const str =
+        'INSERT INTO pets (name, age, type, breed, microchip) VALUES ($1, $2, $3, $4, $5) RETURNING *;';
+    const data = await db.query(str, values);
+    const pet = data.rows[0];
+    return pet;
+};
+
+const updatePetById = async (values) => {
+    const str =
+        'UPDATE pets SET name = $1, age = $2, type = $3, breed = $4, microchip = $5 WHERE id = $6 RETURNING *;';
+    const data = await db.query(str, values);
+    const pet = data.rows[0];
+    return pet;
+};
+
+const deletePetById = async (values) => {
+    const str = 'DELETE from pets WHERE id = $1 RETURNING *;';
+    const data = await db.query(str, values);
+    const pet = data.rows[0];
+    return pet;
+};
+
+module.exports = {
+    getAllPets,
+    getPetById,
+    createPet,
+    updatePetById,
+    deletePetById,
+};
