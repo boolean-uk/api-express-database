f97a009cbb7c82a78e9c774e653c7f7fecaef2b7
Oliver West <oliversimon.west@gmail.com>
2023-03-20 11:48:59 +0000

    books api extension spec implemented

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,7 +1,7 @@
 const booksRepo = require('../repositories/books');
 
 const getAllBooks = async (req, res) => {
-    const { type, topic } = req.query;
+    const { type, topic, author, page, perPage } = req.query;
     let values = [];
     let query = '';
 
@@ -13,16 +13,30 @@ const getAllBooks = async (req, res) => {
         query = 'topic';
         values = [topic];
     }
-
-    const books = await booksRepo.getAllBooks(values, query);
-    res.json({ books });
+    if (author) {
+        query = 'author';
+        values = [author];
+    }
+    if (perPage < 10 || perPage > 50) {
+        return res.status(400).json({
+            error: `parameter invalid perPage: ${perPage} not valid. Accepted range is 10 - 50`,
+        });
+    }
+    const books = await booksRepo.getAllBooks(values, query, page, perPage);
+    const resPerPage = Number(perPage);
+    const resPage = Number(page);
+    res.json({ books, per_page: resPerPage, page: resPage });
 };
 
 const getBookById = async (req, res) => {
     const id = req.params.id;
     const values = [id];
     const book = await booksRepo.getBookById(values);
-    res.json({ book });
+    if (book) {
+        res.json({ book });
+    } else {
+        res.status(404).json({ error: `no book with id: ${id}` });
+    }
 };
 
 const createBook = async (req, res) => {
@@ -36,15 +50,31 @@ const updateBookById = async (req, res) => {
     const id = req.params.id;
     const { title, type, author, topic, publicationDate, pages } = req.body;
     const values = [title, type, author, topic, publicationDate, pages, id];
-    const book = await booksRepo.updateBookById(values);
-    res.status(201).json({ book });
+    const bookIdCheck = await booksRepo.getBookById([id]);
+    if (bookIdCheck) {
+        const bookTitleCheck = await booksRepo.getBookByTitle([title]);
+        if (bookTitleCheck) {
+            return res.status(409).json({
+                error: `A book with the title: ${title} already exists`,
+            });
+        } else {
+            const book = await booksRepo.updateBookById(values);
+            return res.status(201).json({ book });
+        }
+    } else {
+        return res.status(404).json({ error: `no book with id: ${id}` });
+    }
 };
 
 const deleteBookById = async (req, res) => {
     const id = req.params.id;
     const values = [id];
     const book = await booksRepo.deleteBookById(values);
-    res.status(201).json({ book });
+    if (book) {
+        res.status(201).json({ book });
+    } else {
+        res.status(404).json({ error: `no book with id: ${id}` });
+    }
 };
 
 module.exports = {
--- src/repositories/books.js
+++ src/repositories/books.js
@@ -1,6 +1,6 @@
 const db = require('../../db');
 
-const getAllBooks = async (values, query) => {
+const getAllBooks = async (values, query, page = 1, perPage = 20) => {
     let str = 'SELECT * FROM books';
     if (query === 'type') {
         str += ' WHERE type = $1';
@@ -8,10 +8,26 @@ const getAllBooks = async (values, query) => {
     if (query === 'topic') {
         str += ' WHERE topic = $1';
     }
+    if (query === 'author') {
+        str += ' WHERE author = $1';
+    }
+    values.push(perPage);
+    str += ` LIMIT $${values.length}`;
+    if (page > 1) {
+        let pagination = (page - 1) * perPage;
+        str += ` OFFSET ${pagination}`;
+    }
     str += ';';
     const data = await db.query(str, values);
     const books = data.rows;
     return books;
+};
+
+const getBookByTitle = async (values) => {
+    const str = 'SELECT * FROM books WHERE title = $1';
+    const data = await db.query(str, values);
+    const book = data.rows[0];
+    return book;
 };
 
 const getBookById = async (values) => {
@@ -47,6 +63,7 @@ const deleteBookById = async (values) => {
 module.exports = {
     getAllBooks,
     getBookById,
+    getBookByTitle,
     createBook,
     updateBookById,
     deleteBookById,
