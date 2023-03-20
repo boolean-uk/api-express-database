2807fbae47cda97eea536a21bace4f0fe480b8b8
Asiyeyurtkuran <asiyeyurtkuran@gmail.com>
2023-03-20 17:59:35 +0000

    used controllers and repo for books but not finished to get-books test

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,17 +1,128 @@
-// const booksRepo = require('../repositories/books')
+const booksRepo = require('../repositories/books')
 
+// const getAllBooks = async (req, res) => {
+//     const { author, page, per_page } = req.query
+//     let values = [];
+//     let query = ''
+//     if (author || page || per_page) {
+//         const data = await booksRepo.getAllBooks(
+//             author,
+//             page,
+//             per_page
+//         )
+//         res.json({ books: data.rows })
+//     } else {
+//         const data = await getBooks()
+//         res.json({ books: data.rows })
+//     }
+// }
+
+const postBook = async (req, res) => {
+    const { title, type, author, topic, publicationDate, pages } = req.body
+    const values = [title, type, author, topic, publicationDate, pages]
+
+    const data = await booksRepo.postBook(values)
+    return res.status(201).json({ book: data.rows[0] });
+}
+
+const getBookByID = async (req, res) => {
+    const id = Number(req.params.id)
+
+    const values = [id]
+    const data = await booksRepo.getBookByID(values)
+    //res.json({ book: data.rows[0] })
+    if(data) {
+        res.json({data})
+    } else {
+        res.status(404).json({ error: `no book with id: ${id}`})
+    }
+}
+
+const putBook = async (req, res) => { 
+    const id = Number(req.params.id);
+    const { title, type, author, topic, publicationDate, pages } = req.body
+    const values = [id, title, type, author, topic, publicationDate, pages];
+
+    const idControl = await booksRepo.getBookByID([id]);
+    if(idControl){
+        const titleControl = await booksRepo.getBookByTitle([title]);
+        if(titleControl){
+          return res.status(409).json({ error: `A book with the title: ${title} already exists`});
+        } else {
+            const book = await booksRepo.putBook(values);
+            return res.status(201).json({book});
+        }
+    } else {
+        return res.status(404).json({ error: `no book with id: ${id}`})
+    }
+}
+
+ const deleteBookById = async (req, res) => { 
+     const id = Number(req.params.id);
+    const values = [id];
+    const book = await booksRepo.deleteBookById(values);
+
+    if(book) {
+        res.status(201).json({ book });
+    } else {
+        res.status(404).json({ error: `no book with id: ${id}`});
+    }
+
+  
+};
+
+module.exports = {
+    getAllBooks,
+    postBook,
+    getBookByID,
+    putBook,
+    deleteBookById,
+}
+
+
+
+// const booksRepo = require("../repositories/books.js");
+// const db = require("../../db");
 
 // const getBooks = async (req, res) => {
-//     const { type } = req.query
+//   const queries = req.query;
 
-//     let values = []
-//     if(type) {
-//         values = [type]
-//     }
-//     const books = await booksRepo.getBooks(values)
-//     res.json({ books })
+//   if (queries.perPage === undefined) queries.perPage = 20;
+//   if (queries.page === undefined) queries.page = 1;
+//   queries.offset = queries.perPage * (queries.page - 1);
 
-// }
-// module.exports = {
-//     getBooks
-// }
+//   const values = [Number(queries.perPage), queries.offset];
+
+//   if (queries.author !== undefined) {
+//     values.push(queries.author);
+//   }
+
+//   if (queries.perPage < 10 || queries.perPage > 50) {
+//     res.status(400).json({
+//       error: `parameter invalid perPage: ${queries.perPage} not valid. Accepted range is 10 - 50`,
+//     });
+//     return;
+//   }
+
+//   const data = await booksRepo.getBooks(values);
+//   const resdata = { books: data.rows };
+//   if (queries.perPage || queries.page) {
+//     resdata.per_page = Number(queries.perPage);
+//     resdata.page = Number(queries.page);
+//   }
+//   return res.json(resdata);
+// };
+
+
+
+// const getBookById = async (req, res) => {
+//   const id = Number(req.params.id);
+//   const values = [id];
+//   const data = await booksRepo.getBookById(values);
+//   if (data.rowCount === 0) {
+//     res.status(404).json({ error: `no book with id: ${req.params.id}` });
+//     return;
+//   }
+//   return res.json({ book: data.rows[0] });
+// };
+
--- src/repositories/books.js
+++ src/repositories/books.js
@@ -1,21 +1,79 @@
-// const db = require('../../db');
+const db = require('../../db');
 
+// const getAllBooks = async (author, page, per_page) => {
+//     let authors = ''
+//     let pages = 1
+//     let perPage = 20
+//     let filter = ''
+
+//     if (author) {
+//         authors = "AND author = $1"
+//     }
+
+//     if (per_page) {
+//         perPage = 'LIMIT $1'
+//     }
+//     if (page) {
+//         pages = page * perPage
+
+//         return (pages = perPage)
+//     }
+
+//     const bookdata = await db.query("SELECT * FROM books;");
+
+//     return bookdata.rows;
+// }
+
+const postBook = async (values) => {
+    const str = 'INSERT INTO books (title, type, author, topic, "publicationDate", pages ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;';
+    return  await db.query(str, values)
+    // const books = data.rows[0]
+    // res.json({ books })
+}
+
+const getBookByID = async (values) => {
+    const str = 'SELECT * FROM books WHERE id = $1;'
+    const data = await db.query(str, values)
+    const book = data.rows[0]
+    return book;
+}
+const getBookByTitle = async (values) => {
+    const str = 'SELECT * FROM books WHERE title = $1;'
+    const data = await db.query(str, values)
+    const book = data.rows[0]
+    return book;
+}
+const putBook = async (values) => {
+    const str = `UPDATE books SET title = $2, type = $3, author = $4, topic = $5, "publicationDate" = $6, pages = $7 WHERE id = $1 RETURNING *;`;
+    const data = await db.query(str, values)
+    const book = data.rows[0]
+    res.json({ book})
+}
+
+const deleteBookById = async (values) => {
+    const str = 'DELETE FROM  books WHERE id = $1 RETURNING *;';
+    const data = await db.query(str, values);
+    const book = data.rows[0];
+    return book;
+}
+module.exports = {
+    getAllBooks,
+    postBook,
+    getBookByID,
+    putBook,
+    getBookByTitle,
+    deleteBookById
+}
+
+
+// const db = require("../../db");
 
 // const getBooks = async (values) => {
+//   let str = `SELECT * FROM books `;
+//   if (values.length === 3) {
+//     str += `WHERE author = $3 `;
+//   }
+//   str += `LIMIT $1 OFFSET $2 ;`;
+//   return await db.query(str, values);
+// };
 
-//     let str = 'SELECT * FROM books;'
-//     if (values.length !== 0) {
-//         str += 'WHERE type = $1;' //there is a possibility injecting SQL => 'intentionaly' drop table?
-//         //  values = [type]
-//     }
-//     str += ';'
-//     console.log(str)
-//     const data = await db.query(str, values)
-//     console.log(data)
-//     const books = data.rows
-//     return books
-//     // res.json({ books })
-// }
-// module.exports = {
-//     getBooks
-// }
