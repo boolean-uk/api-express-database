d9024332d24e6ed770c40755c2e4392f4caefba7
Gabriel Rosengren <gabbe.rosengren@gmail.com>
2023-06-30 13:56:07 +0200

    started work on first endpoint

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const db = require("../../db");
 
 router.get('/', async (req, res) => {
+  const { type, topic } = req.query
 
+  let string = 'SELECT * FROM books WHERE type = $1'
+
+  if (type === 'Fiction' || type === 'Non-Fiction') {
+    const result = await db.query('SELECT * FROM books WHERE type = $1', [type])
+    return res.json({ books: result.rows })
+  }
+
+  
+  const result = await db.query('SELECT * FROM books')
+  res.json({ books: result.rows })
 })
 
 module.exports = router
