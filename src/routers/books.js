001b7e707f4483a084d150627006793fcf00dda9
Ian Grantham <dev.iangrantham@gmail.com>
2023-06-30 14:35:30 +0100

    set up db

--- src/routers/books.js
+++ src/routers/books.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const db = require("../../db");
 
-router.get('/', async (req, res) => {
-
+router.get('/:type', async (req, res) => {
+  const type = req.params.type
+  const result = await db.query('SELECT * FROM books WHERE type = $1', [type])
+  res.json({ books: result.rows })
 })
 
 module.exports = router
