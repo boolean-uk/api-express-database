fecad8b1bfd9624afd17f53651858e37beef8265
Lee Kelly <leeowenkelly@gmail.com>
2023-06-30 17:47:14 +0100

    core tests passing

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,48 +1,62 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const db = require("../../db");
 
 // GET books
-router.get('/', async (req, res) => {
-    const type = req.query.type
-    const result = await db.query('SELECT * FROM books WHERE type ILIKE $1',[type]);
-    res.json({ books:result.rows })
-})
+router.get("/", async (req, res) => {
+  const { type } = req.query;
+  let result;
+  type
+    ? (result = await db.query("SELECT * FROM books WHERE type ILIKE $1", [
+        type,
+      ]))
+    : (result = await db.query("SELECT * FROM books"));
+  res.json({ books: result.rows });
+});
 
 // POST create a book
-router.post('/', async (req, res) => {
-    const { title, type, author, topic, publicationDate, pages } = req.body;
-    const result = await db.query('INSERT INTO books (title, type, author, topic, "publicationDate", pages)' + 
-    'VALUES ($1, $2, $3, $4, $5, $6)' +
-    'RETURNING *', [title, type, author, topic, publicationDate, pages]);
-    res.status(201).json( { book: result.rows[0]} );
-})
+router.post("/", async (req, res) => {
+  const { title, type, author, topic, publicationDate, pages } = req.body;
+  const result = await db.query(
+    'INSERT INTO books (title, type, author, topic, "publicationDate", pages)' +
+      "VALUES ($1, $2, $3, $4, $5, $6)" +
+      "RETURNING *",
+    [title, type, author, topic, publicationDate, pages]
+  );
+  res.status(201).json({ book: result.rows[0] });
+});
 
 // GET book by ID
-router.get('/:id', async (req, res) => {
-    const id = req.params.id
-    const result = await db.query('SELECT * FROM books WHERE id = $1', [id])
-    res.json({ book: result.rows })
-})
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+  res.json({ book: result.rows[0] });
+});
 
 // PUT update a book
-router.put('/:id', async (req, res) => {
-    const { id } = req.params
-    const { title, type, author, topic, publicationDate, pages } = req.body;
-    const result = await db.query(`UPDATE books
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, type, author, topic, publicationDate, pages } = req.body;
+  const result = await db.query(
+    `UPDATE books
     SET title = $2, type = $3, author = $4, topic = $5, "publicationDate" = $6, pages = $7
     WHERE id = $1
-    RETURNING *`, [id, title, type, author, topic, publicationDate, pages])
-    res.status(201).json( { book: result.rows[0]} )
+    RETURNING *`,
+    [id, title, type, author, topic, publicationDate, pages]
+  );
+  res.status(201).json({ book: result.rows[0] });
 });
 
 // DELETE a book
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    const result = await db.query(`DELETE FROM books
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await db.query(
+    `DELETE FROM books
     WHERE id = $1
-    RETURNING *`, [id])
-    res.status(201).json({ book: result.rows[0]})
-})
+    RETURNING *`,
+    [id]
+  );
+  res.status(201).json({ book: result.rows[0] });
+});
 
-module.exports = router
+module.exports = router;
--- src/routers/pets.js
+++ src/routers/pets.js
@@ -1,50 +1,62 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const db = require("../../db");
 
 // GET pets
-router.get('/', async (req, res) => {
-    const type = req.query.type
-    const result = await db.query('SELECT * FROM pets WHERE type ILIKE $1',[type]);
-    res.json({ pets:result.rows })
-})
-
+router.get("/", async (req, res) => {
+  const { type } = req.query;
+  let result;
+  type
+    ? (result = await db.query("SELECT * FROM pets WHERE type ILIKE $1", [
+        type,
+      ]))
+    : (result = await db.query("SELECT * FROM pets"));
+  res.json({ pets: result.rows });
+});
 
 // POST create a pet
-router.post('/', async (req, res) => {
-    const { name, type, age, breed, microchip} = req.body;
-    const result = await db.query('INSERT INTO pets ( name, type, age, breed, microchip)' + 
-    'VALUES ($1, $2, $3, $4, $5)' +
-    'RETURNING *', [ name, type, age, breed, microchip]);
-    res.status(201).json( { pet: result.rows[0]});
-})
+router.post("/", async (req, res) => {
+  const { name, type, age, breed, microchip } = req.body;
+  const result = await db.query(
+    "INSERT INTO pets ( name, type, age, breed, microchip)" +
+      "VALUES ($1, $2, $3, $4, $5)" +
+      "RETURNING *",
+    [name, type, age, breed, microchip]
+  );
+  res.status(201).json({ pet: result.rows[0] });
+});
 
-// GET pet by ID 
-router.get('/:id', async (req, res) => {
-    const id = req.params.id
-    const result = await db.query('SELECT * FROM pets WHERE id = $1', [id])
-    res.json({ pet: result.rows })
-})
+// GET pet by ID
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await db.query("SELECT * FROM pets WHERE id = $1", [id]);
+  res.json({ pet: result.rows[0] });
+});
 
 // PUT update a pet
-router.put('/:id', async (req, res) => {
-    const { id } = req.params
-    const { name, type, age, breed, microchip} = req.body;
-    const result = await db.query(`UPDATE pets
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, type, age, breed, microchip } = req.body;
+  const result = await db.query(
+    `UPDATE pets
     SET name = $2, type = $3, age = $4, breed = $5, microchip = $6
     WHERE id = $1
-    RETURNING *`, [id, name, type, age, breed, microchip])
-    res.status(201).json( { pet: result.rows[0]} )
+    RETURNING *`,
+    [id, name, type, age, breed, microchip]
+  );
+  res.status(201).json({ pet: result.rows[0] });
 });
 
 // DELETE a pet
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    const result = await db.query(`DELETE FROM pets
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await db.query(
+    `DELETE FROM pets
     WHERE id = $1
-    RETURNING *`, [id])
-    res.status(201).json({ pet: result.rows[0]})
-})
+    RETURNING *`,
+    [id]
+  );
+  res.status(201).json({ pet: result.rows[0] });
+});
 
-
-module.exports = router
+module.exports = router;
