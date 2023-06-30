baf53f23ce46829b5bb83c3ede9a5c00ffebfd50
Gabriel Rosengren <gabbe.rosengren@gmail.com>
2023-06-30 17:00:48 +0200

    started work on gettin book by id

--- src/routers/books.js
+++ src/routers/books.js
@@ -30,11 +30,16 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const { title, type, author, topic, publicationDate, pages } = req.body
-  console.log(req.body)
   const result = await db.query('INSERT INTO books (title, type, author, topic, "publicationDate", pages) ' + 
     'VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', 
     [ title, type, author, topic, publicationDate, pages ])
   return res.status(201).json({ book: result.rows[0] })
+})
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id
+  const result = await db.query('SELECT * FROM books WHERE id = $1', [id])
+  res.json({ book: result.rows })
 })
 
 
