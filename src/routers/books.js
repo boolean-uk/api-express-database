03f120ec87623cf7ffd341c11d29a0b602522415
Ian Grantham <dev.iangrantham@gmail.com>
2023-06-30 15:56:25 +0100

    get books

--- src/routers/books.js
+++ src/routers/books.js
@@ -2,10 +2,29 @@ const express = require('express')
 const router = express.Router()
 const db = require("../../db");
 
-router.get('/:type', async (req, res) => {
-  const type = req.params.type
-  const result = await db.query('SELECT * FROM books WHERE type = $1', [type])
-  res.json({ books: result.rows })
+router.get('/', async (req, res) => {
+  const { type, topic } = req.query
+  let query = 'SELECT * FROM books'
+  const values = []
+  if (type && topic) {
+    values.push(type, topic)
+    query += ' WHERE type ILIKE $1 AND topic ILIKE $2'
+  }
+  if (type && !topic) {
+    values.push(type)
+    query += ' WHERE type ILIKE $1'
+  }
+  if (!type && topic) {
+    values.push(topic)
+    query += ' WHERE topic ILIKE $1'
+  }
+  try {
+    const result = await db.query(query, values)
+    res.json({ books: result.rows})
+  } catch (error) {
+    res.status(500).json({error})
+    console.error(error)
+  }
 })
 
 module.exports = router
