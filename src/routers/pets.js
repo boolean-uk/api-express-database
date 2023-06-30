f1a1dc2ff52a56eac1f0e6e6f24a3ec4f51e4e67
NoroAxper <norikvardanyan.w@gmail.com>
2023-06-30 18:11:59 +0100

    refe

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -22,11 +22,12 @@ router.get('/:id', async (req, res) => {
   res.json({ pet: result.rows[0] })
 })
 
-router.put('/:id', async(req, res) => {
-  const { id } = req.params
-  const {name, age, type, breed, microchip} = req.body
-  const result = await db.query('UPDATE pets SET name = $2, age = $3, type = $4, breed = $5, microchip=$6 WHERE id=$1' + 'RETURNING *', [id, name, age, type, breed, microchip])
-  res.status(201).send({pet: result.rows[0]})
+router.put('/:id', async (req, res) => {
+  const id = req.params.id
+  const { name, age, type, breed, microchip } = req.body
+  const result = await db.query('UPDATE pets SET name = $1, age = $2, type = $3, breed = $4, microchip = $5' + 
+  ' WHERE id = $6 ' + 'RETURNING *', [ name, age, type, breed, microchip, id ])
+  res.status(201).json({ pet: result.rows[0] });
 })
 router.delete('/:id', async(req, res) => {
   const { id } = req.params
