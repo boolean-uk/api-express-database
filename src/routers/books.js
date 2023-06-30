106be1150d7963c2e83e33c34c906c8b47cb3337
Mantas Navickis <navickis@gmail.com>
2023-06-30 17:21:02 +0200

    Get and post in progress

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const router = express.Router()
-const db = require("../../db");
+const db = require('../../db')
 
 router.get('/', async (req, res) => {
-
+  const result = await db.query('SELECT * FROM books')
+  res.json({ books: result.rows })
 })
-
+router.post('/', async (req, res) => {
+  const { title, type, author, topic, publicationdate, pages } = req.body
+  const result = await db.query(
+    'INSERT INTO books (title, type, author, topic, publicationdate, pages)' +
+      'VALUES ($1, $2, $3, $4, $5, $6)' +
+      'RETURNING *',
+    [title, type, author, topic, publicationdate, pages]
+  )
+  res.json({ book: result.rows[0] })
+})
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const result = await db.query('SELECT * FROM books WHERE id = $1', [id])
+  res.json({ book: result.rows })
+})
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.json());
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
 
-//TODO: Implement books and pets APIs using Express Modular Routers
+// TODO: Implement books and pets APIs using Express Modular Routers
 const booksRouter = require('./routers/books.js')
 
 app.use('/books', booksRouter)
