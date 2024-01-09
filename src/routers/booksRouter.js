270ac561b578ebaa2d386d1f0b73b9ad16283e85
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-09 22:03:56 +0000

    Add queries to /books

--- src/routers/booksRouter.js
+++ src/routers/booksRouter.js
@@ -2,8 +2,21 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../db')
 
-router.get('/', async (req, res, next) => {
-  const books = await db.query('SELECT * FROM books')
+router.get('/', async (req, res) => {
+  const { type, topic } = req.query
+  let books = null
+
+  if (type) {
+    books = await db.query('select * from books where type = $1', [type])
+  }
+
+  if (topic) {
+    books = await db.query('select * from books where type = $1', [topic])
+  }
+
+  if (!type && !topic) {
+    books = await db.query('select * from books')
+  }
 
   res.status(200).json({ books: books.rows })
 })
