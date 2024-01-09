4268b7de1099465b24f6f78e7c71cb9c9b460e5f
callumhayden <callum.hayden@gmail.com>
2024-01-09 14:22:00 +0000

    2 test passing, created both get requests for the books

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const db = require("../../db");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  const books = await db.query("SELECT * FROM books");
+  res.json({ books: books.rows });
+});
 
-})
-
-module.exports = router
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+  res.status(200).json({ book: result.rows[0] });
+});
+module.exports = router;
