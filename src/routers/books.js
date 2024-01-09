291445b6f5bb4e03b65f880e9d30b7e923d9891e
Lukas Dembicki <lad_777@hotmail.com>
2024-01-09 17:37:10 +0000

    ADD: POST('/') - Creates a new book and adds to DB (TESTS PASSING)

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const db = require("../../db");
 
-router.get('/', async (req, res) => {
-    const books = await db.query('SELECT * FROM books')
-    res.json({books: books.rows})
-})
+router.get("/", async (req, res) => {
+    const books = await db.query("SELECT * FROM books");
+    res.status(200).json({ books: books.rows });
+});
 
-module.exports = router
+router.post("/", async (req, res) => {
+    const { title, type, author, topic, publication_date, pages } = req.body;
+
+    const newBook = await db.query(
+        "INSERT INTO books (title, type, author, topic, publication_date, pages) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+        [title, type, author, topic, publication_date, pages]
+    );
+
+    return res.status(201).json({ book: newBook.rows[0] });
+});
+
+module.exports = router;
