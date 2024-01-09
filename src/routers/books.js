7d7f714dc3265c3a15a813a071c6ce21ab15f5e3
irlydo <fokum8@gmail.com>
2024-01-09 14:54:49 +0000

    Completed

--- src/routers/books.js
+++ src/routers/books.js
@@ -9,9 +9,38 @@ router.get('/', async (req, res) => {
 
 router.get("/:id", async (req, res) => {
     const { id } = req.params;
-    // The second parameter of the client `query` method takes an array of values
     const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
     res.json({ book: result.rows[0] });
   });
+
+  router.post('/', async (req, res) => {
+    const { title, type, author, topic, publication_date, pages } = req.body;
+  
+    const newBook = await db.query(
+      'INSERT INTO books (title, type, author, topic, publication_date, pages) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+      [title, type, author, topic, publication_date, pages]
+    )
+    res.status(201).json({ book: newBook.rows[0] })
+  })
+
+  router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, type, author, topic, publication_date, pages } = req.body;
+  
+    const updatedBook = await db.query(
+      'UPDATE books SET title = $2, type = $3, author = $4, topic = $5, publication_date = $6, pages = $7 WHERE id = $1 RETURNING *',
+      [id, title, type, author, topic, publication_date, pages]
+    )
+  
+    res.status(201).json({ book: updatedBook.rows[0]})
+  })
+
+  router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    const deletedBook = await db.query('DELETE FROM books WHERE id = $1 RETURNING *', [id]);
+  
+    res.status(201).json({ book: deletedBook.rows[0] })
+  })
 
 module.exports = router
