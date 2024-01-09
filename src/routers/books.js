6092d1a6d1a2baec75528dd06097f9c93b2d821b
Lukas Dembicki <lad_777@hotmail.com>
2024-01-09 17:13:13 +0000

    ADD: router.get('/') - Retrieves all books from DB.

--- src/routers/books.js
+++ src/routers/books.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const db = require("../../db");
 
 router.get('/', async (req, res) => {
-
+    const books = await db.query('SELECT * FROM books')
+    res.json({books: books.rows})
 })
 
 module.exports = router
--- src/routers/pets.js
+++ src/routers/pets.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const db = require("../../db");
 
 router.get('/', async (req, res) => {
-
+    
 })
 
 module.exports = router
