4661ef936e022532a84e4691d754cc87e33d46eb
callumhayden <callum.hayden@gmail.com>
2024-01-09 14:34:30 +0000

    first post request for books has been added

--- src/routers/books.js
+++ src/routers/books.js
@@ -12,4 +12,14 @@ router.get("/:id", async (req, res) => {
   const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
   res.status(200).json({ book: result.rows[0] });
 });
+
+router.post("/", async (req, res) => {
+    const { title, type, author, topic, publication_date, pages } = req.body;
+    const newBook = await db.query(
+      "INSERT INTO books (title, type, author, topic, publication_date, pages) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [title, type, author, topic, publication_date, pages]
+    );
+    res.status(201).json({ book: newBook.rows[0] });
+  });
+  
 module.exports = router;
