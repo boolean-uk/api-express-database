62898b30619c04fbdb0efeb95f1aeb9de06b3d18
callumhayden <callum.hayden@gmail.com>
2024-01-09 14:50:51 +0000

    created the second get request for a specific id. Test is passing

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -7,5 +7,9 @@ router.get("/", async (req, res) => {
     res.json({ pets: pets.rows });
   });
 
-
+  router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const result = await db.query("SELECT * FROM pets WHERE id = $1", [id]);
+    res.status(200).json({ pet: result.rows[0] });
+  });
 module.exports = router;
