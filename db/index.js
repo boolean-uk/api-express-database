dd2e33f92fce1a6ede87f6268e75d6c1e57e332c
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-09 16:59:34 +0000

    refactor: added typedef to client.query

--- db/index.js
+++ db/index.js
@@ -1,25 +1,32 @@
 // Load our .env file
-require('dotenv').config()
+require("dotenv").config();
 
 // Require Client obj from the postgres node module
 const { Client } = require("pg");
 
 const client = {
-  query: async (str, values) => {
+  /**
+   * @param {Object} query
+   * @param {String} query.name
+   * @param {String} query.text
+   * @param {any[]} query.values
+   * @return {Promise<import('pg').QueryResult>}
+   */
+  query: async (query) => {
     // Get the connection string from process.env -
     // the dotenv library sets this variable based
     // on the contents of our env file
     // Create a new connection to the database using the Client
     // object provided by the postgres node module
-    const dbClient = new Client(process.env.PGURL)
+    const dbClient = new Client(process.env.PGURL);
     // connect a connection
-    await dbClient.connect()
+    await dbClient.connect();
     // execute the query
-    const result = await dbClient.query(str, values)
+    const result = await dbClient.query(query);
     // close the connection
-    await dbClient.end()
-    return result
-  }
-}
+    await dbClient.end();
+    return result;
+  },
+};
 
 module.exports = client;
