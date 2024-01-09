16a06327a26064abd00b7c26b5c85f7dc4ba8309
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-09 18:34:17 +0000

    feat: add select statement builder

---
+++ src/statements/statementBuild.js
@@ -0,0 +1,32 @@
+/**
+  @typedef { "SELECT" | "INSERT" | "UPDATE" | "DELETE" } Statement
+  @typedef { "books" | "pets" } Table
+  @typedef { { text: String, values: ( Number | String | Boolean )[] } } Query
+ */
+
+/**
+ *
+ * @param { Table } table
+ * @param { {} } [filter] - key value pairs of filters to apply eg: { type: "dog", name: "steve" }
+ * @returns { Query }
+ */
+function select(table, filter) {
+  const queryText = "SELECT * FROM $1"
+  let valueCount = 1
+  const values = []
+  values.push(table)
+
+  let whereText = ""
+  if (filter) {
+    whereText = " WHERE"
+    Object.entries(filter).forEach(([key, value], index) => {
+      whereText += `${index > 0 ? " AND" : ""} $${++valueCount} = $${++valueCount}`
+      values.push(key, value)
+    })
+  }
+
+  return {
+    text: queryText + whereText,
+    values
+  }
+}
