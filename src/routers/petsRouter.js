c997cf2a56b4e795e1fad324830cd32dae5a8e5b
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-09 22:32:17 +0000

    Add GET endpoint /pets

--- src/routers/petsRouter.js
+++ src/routers/petsRouter.js
@@ -2,6 +2,20 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../db')
 
-router.get('/', async (req, res) => {})
+router.get('/', async (req, res, next) => {
+  const { type } = req.query
+
+  let pets = null
+
+  if (type) {
+    pets = await db.query('select * from pets where type = $1', [type])
+  }
+
+  if (!type) {
+    pets = await db.query('select * from pets')
+  }
+
+  res.status(200).json({ pets: pets.rows })
+})
 
 module.exports = router
