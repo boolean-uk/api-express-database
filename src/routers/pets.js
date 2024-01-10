08a6d469c6842cb722a8035739df4fe4c827d5b3
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-10 21:54:49 +0100

    remove console.log

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -5,7 +5,7 @@ const extractParams = require('../helper')
 
 router.get('/', async (req, res) => {
   const pets = await db.query("SELECT * FROM pets")
-  res.json( {  pets: pets.rows })
+  res.json( { pets: pets.rows })
 })
 
 router.get('/:id', async ( req, res ) => {
@@ -26,7 +26,6 @@ router.put('/:id', async (req, res) => {
   const { id } = req.params
   const { name, age, type, breed, has_microchip } = req.body
   const values = [id, name, age, type, breed, has_microchip]
-  console.log(values)
   const pet = await db.query('UPDATE pets SET name = $2, age = $3, type = $4, breed = $5, has_microchip = $6 WHERE id = $1 RETURNING *', values)
 
   res.status(201).json( { pet: pet.rows[0] })
