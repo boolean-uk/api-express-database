65c0600d7c0f8f3d39bfe0a54fd159c989edf76e
Kye <NovusAa12@proton.me>
2024-01-10 19:37:37 +0000

    add controllers for pets router

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -1,53 +1,48 @@
 const express = require("express");
+const {
+  getAllPets,
+  createPet,
+  getPetById,
+  deletePet,
+  updatePet,
+} = require("../controllers/pets");
+
 const router = express.Router();
-const db = require("../../db");
 
 router.get("/", async (req, res) => {
-  const pets = await db.query("SELECT * FROM pets");
+  const allPets = await getAllPets();
 
-  return res.status(200).send({ pets: pets.rows });
+  return res.status(200).send({ pets: allPets });
 });
 
 router.post("/", async (req, res) => {
-  const { name, age, type, breed, has_microchip } = req.body;
+  const newPet = await createPet(req.body);
 
-  const newPet = await db.query(
-    "INSERT INTO pets (name, age, type, breed, has_microchip) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-    [name, age, type, breed, has_microchip]
-  );
-
-  return res.status(201).send({ pet: newPet.rows[0] });
+  return res.status(201).send({ pet: newPet });
 });
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
 
-  const pet = await db.query("SELECT * FROM pets WHERE id = $1", [id]);
+  const petById = await getPetById(id);
 
-  return res.status(200).send({ pet: pet.rows[0] });
+  return res.status(200).send({ pet: petById });
 });
 
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
-  const { name, age, type, breed, has_microchip } = req.body;
 
-  const updatedPet = await db.query(
-    "UPDATE pets SET name = $1, age = $2, type = $3, breed = $4, has_microchip = $5 WHERE id = $6 RETURNING *",
-    [name, age, type, breed, has_microchip, id]
-  );
+  const updatedPet = await updatePet(id, req.body);
 
-  return res.status(201).send({ pet: updatedPet.rows[0] });
+  return res.status(201).send({ pet: updatedPet });
 });
 
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
 
-  const deletedPet = await db.query(
-    "DELETE FROM pets WHERE id = $1 RETURNING *",
-    [id]
-  );
+  const deletedPet = await deletePet(id);
 
-  return res.status(201).send({ pet: deletedPet.rows[0] });
+  return res.status(201).send({ pet: deletedPet });
 });
 
 module.exports = router;
