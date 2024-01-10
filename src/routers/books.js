e3bc00ffa306f3ac8deac241430a10c9a2dc2473
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-10 10:03:59 +0100

    first endpoint configured

--- src/routers/books.js
+++ src/routers/books.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const db = require("../../db");
 
 router.get('/', async (req, res) => {
-
+  const books = await db.query('SELECT * from books')
+  res.json({ books })
 })
 
 module.exports = router
