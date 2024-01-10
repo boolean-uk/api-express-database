a4ced31ff73e6eda866767554538b211a59a12f7
Lukas Dembicki <lad_777@hotmail.com>
2024-01-10 11:46:01 +0000

    ADD: GET('/') - Retrieves all pets.

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 const db = require("../../db");
 
+const { getAllPets } = require('../controllers/pets.js')
+
 router.get('/', async (req, res) => {
-    
+    const pets = await getAllPets()
+    res.status(200).json({pets: pets})
 })
 
 module.exports = router
