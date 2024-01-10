76a93e7498347f331695b612749f07f42ff159e0
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-10 15:32:00 +0100

    fix put book endpoint

--- src/routers/books.js
+++ src/routers/books.js
@@ -40,8 +40,7 @@ router.put('/:id', async (req, res) => {
 
   const values = [id, title, type, author, topic, publication_date, pages]
 
-  const book = await db.query('UPDATE books SET title = $2, type = $3, author = $4, publication_date = $5, pages = $6 WHERE id = $1 RETURNING *', values)
-  console.log(book)
+  const book = await db.query('UPDATE books SET title = $2, type = $3, author = $4, topic = $5, publication_date = $6, pages = $7 WHERE id = $1 RETURNING *', values)
 
   res.json( { book: book.rows[0] } )
 })
