44e7498f582a80364def6e6794de5edb2584616d
Lukas Dembicki <lad_777@hotmail.com>
2024-01-10 12:38:35 +0000

    ADD: GET('/:id') - Retrieves pet by ID.

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const db = require("../../db");
 
-const { getAllPets, addNewPet } = require('../controllers/pets.js')
+const { getPets, addNewPet } = require('../controllers/pets.js')
 
 router.get('/', async (req, res) => {
-    const pets = await getAllPets()
+    const pets = await getPets()
     res.status(200).json({pets: pets})
 })
 
@@ -13,4 +13,11 @@ router.post('/', async (req, res) => {
     const newPet = await addNewPet(req.body)
     res.status(201).json({pet: newPet})
 })
+
+router.get('/:id', async (req, res) => {
+    const foundPet = await getPets(req.params)
+    res.status(200).json({pet: foundPet})
+})
+
+
 module.exports = router
