0df7392590660ac9496c9de805b7d4c948e05412
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-10 17:47:48 +0000

    feat: add getAllBooks()

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -2,3 +2,13 @@ const books = require("../repositories/books.repository.js");
 
 const Types = require("../types.d.js");
 
+/**
+ *
+ * @param { Types.Request } req
+ * @param { Types.Response } res
+ * @returns { void }
+ */
+function getAllBooks(req, res) {
+  const response = books.getBooks(req.query)
+  res.json(response)
+}
