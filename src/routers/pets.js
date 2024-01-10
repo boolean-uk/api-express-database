b81ee0182936ea5458716d1035ae0d924e8deb96
Lukas Dembicki <lad_777@hotmail.com>
2024-01-10 13:09:49 +0000

    ADD; PUT('/:id') - Calls updatePetById and updates a pet.

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require("../../db");
 
-const { getPets, addNewPet } = require('../controllers/pets.js')
+const { getPets, addNewPet, updatePetById } = require('../controllers/pets.js')
 
 router.get('/', async (req, res) => {
     const pets = await getPets()
@@ -18,6 +18,12 @@ router.get('/:id', async (req, res) => {
     const foundPet = await getPets(req.params)
     res.status(200).json({pet: foundPet})
 })
+
+router.put('/:id', async (req, res) => {
+    const updatePet = await updatePetById(req.body, req.params)
+    res.status(201).json({pet: updatePet})
+})
+
 
 
 module.exports = router
