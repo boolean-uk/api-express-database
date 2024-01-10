fd3c8f85bf96e3543005cc09ba9755610eccec90
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-10 10:42:30 +0100

    add parameters to getBooks

--- src/routers/books.js
+++ src/routers/books.js
@@ -3,8 +3,24 @@ const router = express.Router()
 const db = require("../../db");
 
 router.get('/', async (req, res) => {
-  const books = await db.query('SELECT * from books')
-  res.json({ books })
+  const { type, topic } = req.query
+  // const queryParamsArr = [ type, topic ].filter((param) => !!param === true)
+  const queryParamsArr = Object.keys(req.query)
+  console.log(queryParamsArr)
+
+  let queryStr = 'SELECT * from books'
+
+  if (queryParamsArr) {
+    queryParamsArr.forEach((param, index) => {
+      const keywordPrefix = index === 0 ? "WHERE" : "AND"
+      queryStr = queryStr.concat(" ", keywordPrefix)
+      queryStr = queryStr.concat(" ", `${param} = '${req.query[param]}'`)
+    })
+  }
+
+  console.log(queryStr)
+  const books = await db.query(queryStr)
+  res.json({ books: books.rows })
 })
 
 module.exports = router
