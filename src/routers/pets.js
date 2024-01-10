323ec52ccb11427af0314430a202adf378fa9485
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-10 21:15:59 +0100

    add PUT endpoint to pets

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -22,6 +22,16 @@ router.post('/', async (req, res) => {
   res.json( { pet: pet.rows[0] })
 })
 
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const { name, age, type, breed, has_microchip } = req.body
+  const values = [id, name, age, type, breed, has_microchip]
+  console.log(values)
+  const pet = await db.query('UPDATE pets SET name = $2, age = $3, type = $4, breed = $5, has_microchip = $6 WHERE id = $1 RETURNING *', values)
+
+  res.json( { pet: pet.rows[0] })
+})
+
 router.delete('/:id', async ( req, res ) => {
   const { id } = req.params
   const pets = await db.query('DELETE FROM pets WHERE id = $1 RETURNING *', [id])
