5e3084c73a7e7da61a4423eefe1475879585add7
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-10 15:07:50 +0000

    chore: remove unecessary returns

--- src/routers/books.js
+++ src/routers/books.js
@@ -5,29 +5,29 @@ const { getAllBooks, addBook, getBookBy, deleteBook, editBook } = require("../co
 
 router.get("/", async (req, res) => {
   const result = await getAllBooks();
-  return res.json({ books: result.rows });
+  res.json({ books: result.rows });
 });
 
 router.post("/", async (req, res) => {
   await addBook(req.body);
   const result = await getBookBy({ title: req.body.title });
-  return res.status(201).json({ book: result.rows[0] });
+  res.status(201).json({ book: result.rows[0] });
 });
 
 router.get("/:id", async (req, res) => {
   const result = await getBookBy(req.params);
-  return res.json({ book: result.rows[0] });
+  res.json({ book: result.rows[0] });
 });
 
 router.delete("/:id", async (req, res) => {
   const itemToDelete = await deleteBook(req.params)
-  return res.status(201).json({ book: itemToDelete.rows[0] });
+  res.status(201).json({ book: itemToDelete.rows[0] });
 });
 
 router.put("/:id", async (req, res) => {
   await editBook(req.params, req.body)
   const result = await getBookBy(req.params);
-  return res.status(201).json({ book: result.rows[0] });
+  res.status(201).json({ book: result.rows[0] });
 });
 
 module.exports = router;
--- src/routers/pets.js
+++ src/routers/pets.js
@@ -11,29 +11,29 @@ const {
 
 router.get("/", async (req, res) => {
   const result = await getAllPets();
-  return res.json({ pets: result.rows });
+  res.json({ pets: result.rows });
 });
 
 router.post("/", async (req, res) => {
   await addPet(req.body);
   const result = await getPetBy({ name: req.body.name });
-  return res.status(201).json({ pet: result.rows[0] });
+  res.status(201).json({ pet: result.rows[0] });
 });
 
 router.get("/:id", async (req, res) => {
   const result = await getPetBy(req.params);
-  return res.json({ pet: result.rows[0] });
+  res.json({ pet: result.rows[0] });
 });
 
 router.delete("/:id", async (req, res) => {
   const toBeDeleted = await deletePet(req.params);
-  return res.status(201).json({ pet: toBeDeleted.rows[0] });
+  res.status(201).json({ pet: toBeDeleted.rows[0] });
 });
 
 router.put("/:id", async (req, res) => {
   await editPet(req.params, req.body);
   const result = await getPetBy(req.params);
-  return res.status(201).json({ pet: result.rows[0] });
+  res.status(201).json({ pet: result.rows[0] });
 });
 
 module.exports = router;
