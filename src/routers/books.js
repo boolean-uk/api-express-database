9cdcd20bba00599a972aecd94d13e76bb042cabb
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-10 12:32:13 +0100

    add pets router

--- src/routers/books.js
+++ src/routers/books.js
@@ -3,22 +3,20 @@ const router = express.Router()
 const db = require("../../db");
 
 router.get('/', async (req, res) => {
-  const { type, topic } = req.query
   // const queryParamsArr = [ type, topic ].filter((param) => !!param === true)
-  const queryParamsArr = Object.keys(req.query)
-  console.log(queryParamsArr)
+  const queryParamsKeys = Object.keys(req.query)
+  console.log(queryParamsKeys)
 
   let queryStr = 'SELECT * from books'
 
-  if (queryParamsArr) {
-    queryParamsArr.forEach((param, index) => {
+  if (queryParamsKeys) {
+    queryParamsKeys.forEach((param, index) => {
       const keywordPrefix = index === 0 ? "WHERE" : "AND"
       queryStr = queryStr.concat(" ", keywordPrefix)
       queryStr = queryStr.concat(" ", `${param} = '${req.query[param]}'`)
     })
   }
 
-  console.log(queryStr)
   const books = await db.query(queryStr)
   res.json({ books: books.rows })
 })
