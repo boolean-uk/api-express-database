a25bf690644beb1dd4fdaf437b3284828988fc4d
Satoki <satokiito@gmail.com>
2024-01-10 12:04:17 +0000

    ext. 1 start - added author query param

--- src/routers/books.js
+++ src/routers/books.js
@@ -4,31 +4,32 @@ const db = require("../../db");
 
 // GET ALL BOOKS (INC. QUERY PARAMS)
 router.get('/', async (req, res) => {
-    const { type, topic } = req.query
+    const { type, topic, author } = req.query
 
-    if (type && topic) {
-        const books = await db.query(
-            'SELECT * FROM books WHERE type = $1 AND topic = $2',
-            [type, topic])
-        return res.status(200).json({ books: books.rows })
-    }
-
+    let books
     if (type) {
-        const books = await db.query(
+        books = await db.query(
             'SELECT * FROM books WHERE type = $1',
             [type])
         return res.status(200).json({ books: books.rows })
     }
 
     if (topic) {
-        const books = await db.query(
+        books = await db.query(
             'SELECT * FROM books WHERE topic = $1',
             [topic]
         )
         return res.status(200).json({ books: books.rows })
     }
 
-    const books = await db.query('SELECT * FROM books')
+    if (author) {
+        books = await db.query(
+            'SELECT * FROM books WHERE author = $1',
+            [author])
+        return res.status(200).json({ books: books.rows })
+    }
+
+    books = await db.query('SELECT * FROM books')
     return res.status(200).json({ books: books.rows })
 })
 
