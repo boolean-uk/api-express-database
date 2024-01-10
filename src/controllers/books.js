624870e6a8cbcd32373b874a02aa71ec9c70cb50
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-10 12:40:05 +0000

    refactor: extract book.js controllers

---
+++ src/controllers/books.js
@@ -0,0 +1,62 @@
+const db = require("../../db");
+
+const getAllBooks = async () => {
+  const result = await db.query("SELECT * FROM books");
+  return result;
+};
+const getBookBy = async (request_param) => {
+  const key = Object.keys(request_param)[0];
+  const value = Object.values(request_param)[0];
+  const base_query_string = "SELECT * FROM books WHERE ";
+  let query_string;
+  if (key === "id") {
+    query_string = base_query_string.concat("id = $1");
+  }
+  if (key === "title") {
+    query_string = base_query_string.concat("title = $1");
+  }
+  const result = await db.query(query_string, [value]);
+  return result;
+};
+const deleteBook = async (request_param) => {
+  const itemToDelete = await getBookBy(request_param);
+  const { id } = request_param;
+  await db.query("DELETE FROM books WHERE id = $1", [id]);
+  return itemToDelete;
+};
+const addBook = async (book) => {
+  const properties = Object.keys(book);
+  const values = Object.values(book);
+  const query_string =
+    "INSERT INTO books ".concat(`(${properties.map((p) => " ".concat(p))})`) +
+    " VALUES ( $1, $2, $3, $4, $5, $6)";
+  await db.query(
+    query_string,
+    values.map((value) => value)
+  );
+};
+const editBook = async (request_param, request_body) => {
+  const { id } = request_param;
+  const properties = Object.keys(request_body);
+  const values = Object.values(request_body);
+  values.push(id);
+  const base_query_string = "UPDATE books SET ";
+  const query_string_updates = properties.map((property, index) =>
+    property.concat(` = $${index + 1} `)
+  );
+  const query_string_where = " WHERE id = $7";
+  const full_query_string =
+    base_query_string + query_string_updates + query_string_where;
+  await db.query(
+    full_query_string,
+    values.map((value) => value)
+  );
+};
+
+module.exports = {
+  getAllBooks,
+  getBookBy,
+  deleteBook,
+  addBook,
+  editBook,
+};
--- src/routers/books.js
+++ src/routers/books.js
@@ -1,45 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db");
+const { getAllBooks, addBook, getBookBy, deleteBook, editBook } = require("../controllers/books");
 
 router.get("/", async (req, res) => {
-  const result = await db.query("SELECT * FROM books");
+  const result = await getAllBooks();
   return res.json({ books: result.rows });
 });
 
 router.post("/", async (req, res) => {
-  const { title, type, author, topic, publication_date, pages } = req.body;
-  await db.query(
-    "INSERT INTO books (title, type, author, topic, publication_date, pages) VALUES ( $1, $2, $3, $4, $5, $6);",
-    [title, type, author, topic, publication_date, pages]
-  );
-  const result = await db.query("SELECT * FROM books WHERE title = $1", [
-    title,
-  ]);
+  await addBook(req.body);
+  const result = await getBookBy({ title: req.body.title });
   return res.status(201).json({ book: result.rows[0] });
 });
 
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+  const result = await getBookBy(req.params);
   return res.json({ book: result.rows[0] });
 });
 
 router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const itemToDelete = await db.query("SELECT * FROM books WHERE id = $1", [id]);
-  await db.query("DELETE FROM books WHERE id = $1", [id]);
+  const itemToDelete = await deleteBook(req.params)
   return res.status(201).json({ book: itemToDelete.rows[0] });
 });
 
 router.put("/:id", async (req, res) => {
-  const { id } = req.params
-  const { title, type, author, topic, publication_date, pages } = req.body;
-  await db.query(
-    "UPDATE books SET title = $1, type = $2, author = $3, topic =  $4, publication_date = $5, pages = $6 WHERE id = $7",
-    [title, type, author, topic, publication_date, pages, id]
-  );
-  const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+  await editBook(req.params, req.body)
+  const result = await getBookBy(req.params);
   return res.status(201).json({ book: result.rows[0] });
 });
 
