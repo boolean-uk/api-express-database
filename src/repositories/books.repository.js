c0fac61b89d2e066a618719c56f2876a58afb321
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-10 16:09:21 +0000

    feat: add getBooks()

--- src/repositories/books.repository.js
+++ src/repositories/books.repository.js
@@ -3,3 +3,17 @@ const stmtHelper = require("../helpers/statementHelpers.js");
 
 const Types = require("../types.d.js");
 
+/**
+ *
+ * @param { Types.FilterValues } [filter]
+ * @returns { Promise<Object[]> }
+ */
+async function getBooks(filter) {
+  const stmt = stmtHelper.selectStmt("books", filter);
+  const result = await db.query(stmt);
+  return result.rows;
+}
+
+module.exports = {
+  getBooks
+}
