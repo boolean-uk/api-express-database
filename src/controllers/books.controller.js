fcbcf478b0efb7e432942a30636da902f1cc49ce
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-10 17:51:11 +0000

    fix: convert function to async

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -6,9 +6,9 @@ const Types = require("../types.d.js");
  *
  * @param { Types.Request } req
  * @param { Types.Response } res
- * @returns { void }
+ * @returns { Promise<void> }
  */
-function getAllBooks(req, res) {
-  const response = books.getBooks(req.query)
-  res.json(response)
+async function getAllBooks(req, res) {
+  const response = await books.getBooks(req.query);
+  res.json(response);
 }
