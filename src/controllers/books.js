e2497158096ece7797eb28a6e41057aaefebdb05
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-10 16:15:01 +0000

    refactor: add repositories for books

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,56 +1,26 @@
-const db = require("../../db");
-
-const getAllBooks = async () => {
-  const result = await db.query("SELECT * FROM books");
-  return result;
+const booksRepository = require("../repositories/books")
+const getAllBooks = async (req, res) => {
+  const result = await booksRepository.getAllBooks()
+  res.json({ books: result.rows });
 };
-const getBookBy = async (request_param) => {
-  const key = Object.keys(request_param)[0];
-  const value = Object.values(request_param)[0];
-  const base_query_string = "SELECT * FROM books WHERE ";
-  let query_string;
-  if (key === "id") {
-    query_string = base_query_string.concat("id = $1");
-  }
-  if (key === "title") {
-    query_string = base_query_string.concat("title = $1");
-  }
-  const result = await db.query(query_string, [value]);
-  return result;
+const getBookBy = async (req, res) => {
+  const result = await booksRepository.getBookBy(req.params);
+  res.json({ book: result.rows[0] });
 };
-const deleteBook = async (request_param) => {
-  const itemToDelete = await getBookBy(request_param);
-  const { id } = request_param;
-  await db.query("DELETE FROM books WHERE id = $1", [id]);
-  return itemToDelete;
+const deleteBook = async (req, res) => {
+  const itemToDelete = await booksRepository.deleteBook(req.params)
+  res.status(201).json({ book: itemToDelete.rows[0] });
 };
-const addBook = async (book) => {
-  const properties = Object.keys(book);
-  const values = Object.values(book);
-  const query_string =
-    "INSERT INTO books ".concat(`(${properties.map((p) => " ".concat(p))})`) +
-    " VALUES ( $1, $2, $3, $4, $5, $6)";
-  await db.query(
-    query_string,
-    values.map((value) => value)
-  );
+const addBook = async (req, res) => {
+  await booksRepository.addBook(req.body)
+  const result = await booksRepository.getBookBy({ title: req.body.title });
+  res.status(201).json({ book: result.rows[0] });
 };
-const editBook = async (request_param, request_body) => {
-  const { id } = request_param;
-  const properties = Object.keys(request_body);
-  const values = Object.values(request_body);
-  values.push(id);
-  const base_query_string = "UPDATE books SET ";
-  const query_string_updates = properties.map((property, index) =>
-    property.concat(` = $${index + 1} `)
-  );
-  const query_string_where = " WHERE id = $7";
-  const full_query_string =
-    base_query_string + query_string_updates + query_string_where;
-  await db.query(
-    full_query_string,
-    values.map((value) => value)
-  );
+const editBook = async (req, res) => {
+  await booksRepository.editBook(req.params, req.body)
+  await booksRepository.editBook(req.params, req.body)
+  const result = await booksRepository.getBookBy(req.params);
+  res.status(201).json({ book: result.rows[0] });
 };
 
 module.exports = {
--- src/routers/books.js
+++ src/routers/books.js
@@ -1,33 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../../db");
 const { getAllBooks, addBook, getBookBy, deleteBook, editBook } = require("../controllers/books");
+const booksControllers = require("../controllers/books")
 
-router.get("/", async (req, res) => {
-  const result = await getAllBooks();
-  res.json({ books: result.rows });
-});
+router.get("/",  booksControllers.getAllBooks);
 
-router.post("/", async (req, res) => {
-  await addBook(req.body);
-  const result = await getBookBy({ title: req.body.title });
-  res.status(201).json({ book: result.rows[0] });
-});
+router.post("/", booksControllers.addBook)
 
-router.get("/:id", async (req, res) => {
-  const result = await getBookBy(req.params);
-  res.json({ book: result.rows[0] });
-});
+router.get("/:id", booksControllers.getBookBy);
 
-router.delete("/:id", async (req, res) => {
-  const itemToDelete = await deleteBook(req.params)
-  res.status(201).json({ book: itemToDelete.rows[0] });
-});
+router.delete("/:id", booksControllers.deleteBook);
 
-router.put("/:id", async (req, res) => {
-  await editBook(req.params, req.body)
-  const result = await getBookBy(req.params);
-  res.status(201).json({ book: result.rows[0] });
-});
+router.put("/:id", booksControllers.editBook);
 
 module.exports = router;
