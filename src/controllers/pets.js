46e1f001198b6f9804b13f337d9c19fdf114261e
Lukas Dembicki <lad_777@hotmail.com>
2024-01-10 12:39:11 +0000

    ADD: getPets() - renamed and added new statement to handle finding pet by ID.

--- src/controllers/pets.js
+++ src/controllers/pets.js
@@ -4,23 +4,32 @@ let pets;
 let select_query = "SELECT * FROM pets";
 const sqlPetKeys = "name, age, type, breed, has_microchip";
 
-const getAllPets = async () => {
-    pets = await db.query(select_query);
-    return pets.rows;
+const getPets = async (req_params_or_query) => {
+
+    if (!req_params_or_query) {
+        pets = await db.query(select_query);
+        return pets.rows;
+    }
+
+    if (req_params_or_query){
+        const { id } = req_params_or_query
+        pets = await db.query(select_query.concat(' WHERE id = $1'), [id])
+        return pets.rows[0]
+    }
 };
 
 const addNewPet = async (req_body) => {
     const { name, age, type, breed, has_microchip } = req_body;
-    
+
     const newPet = await db.query(
         `INSERT INTO pets (${sqlPetKeys}) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
         [name, age, type, breed, has_microchip]
-        );
-        
+    );
+
     return newPet.rows[0];
 };
 
 module.exports = {
-    getAllPets,
+    getPets,
     addNewPet,
 };
