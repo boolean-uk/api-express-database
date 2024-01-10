65787d15ab87e73fee73f4c2319b5ffcd810be09
Satoki <satokiito@gmail.com>
2024-01-10 18:19:44 +0000

    Breeds repository function set up

--- src/controllers/breeds.js
+++ src/controllers/breeds.js
@@ -1,12 +1,10 @@
 const db = require("../../db");
 
+const getAllBreedsRep = require('../repositories/breeds.js')
+
 const getAllBreeds = async (req, res) => {
     const { type } = req.query
-    let select_query = 'SELECT DISTINCT breed FROM pets'
-    let breed
-    breed = await db.query(
-        select_query.concat(' WHERE type = $1'),
-    [type])
+    let breed = await getAllBreedsRep(type)
     return res.status(200).json({ breeds: breed.rows })
 }
 
