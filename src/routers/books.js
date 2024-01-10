b4db192097d171c8e67cf10e3394e2c102668279
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-10 18:08:09 +0000

    style: prettier format

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const booksController = require("../controllers/books.controller.js")
+const booksController = require("../controllers/books.controller.js");
 
-router.get('/', booksController.getAllBooks)
+router.get("/", booksController.getAllBooks);
 
-module.exports = router
+module.exports = router;
