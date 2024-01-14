bf15194cc3fd7f7d4211f2d05abb20ee1cd5f684
Elizabeth <lizbeepmc@gmail.com>
2024-01-14 12:37:01 +0300

    post a book

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,7 +1,16 @@
 const db = require("../../db/index.js");
-const getAllBooks = async() => {
-    const books = await db.query('SELECT * FROM books');
-    return books.rows
+const getAllBooks = async () => {
+  const books = await db.query("SELECT * FROM books");
+  return books.rows;
+};
+const postBookById = async ( request_body) => {
+  const { title, type, author, topic, publication_date, pages } = request_body;
 
-}
-module.exports = {getAllBooks}
+  // RETURNING keyword is for INSERT, UPDATE and DELETE queries to show us what row of data has been created/updated/deleted
+  const newBook = await db.query(
+    "INSERT INTO books (title, type, author, topic, publication_date, pages) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    [title, type, author, topic, publication_date, pages]
+  )
+  return newBook.rows[0]
+};
+module.exports = { getAllBooks, postBookById };
--- src/routers/books.js
+++ src/routers/books.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllBooks } = require("../controllers/books");
+const { getAllBooks, postBookById } = require("../controllers/books");
 
 router.get("/", async (req, res) => {
   const books = await getAllBooks();
   console.log('Books:', books);
   res.status(200).json({ books: books });
 });
-router.post("/", async (req, res) => {
-
-});
+router.post('/', async (req, res) => {
+  // [POST] localhost:3030/movies
+  const book = await postBookById(req.body);
+  res.status(201).json({ book: book })
+})
 
 module.exports = router;
