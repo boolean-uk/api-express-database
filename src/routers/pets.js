1175b91265ad11a16abb38d4f17e1883568836a1
aayushlama4008 <aayushlama4008@gmail.com>
2024-01-15 02:02:30 +0100

    added get req for pets

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -2,5 +2,9 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db");
 
+router.get('/', async (req,res)=>{
+    const pets = await db.query('SELECT * FROM pets')
+    res.json({pets: pets.rows})
+})
 
 module.exports = router;
