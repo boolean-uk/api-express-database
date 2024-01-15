fb302b5864b6b51bbb53795bf6b7dac1d98acff9
aayushlama4008 <aayushlama4008@gmail.com>
2024-01-15 02:03:57 +0100

    added get req by id

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db");
 
-router.get('/', async (req,res)=>{
-    const pets = await db.query('SELECT * FROM pets')
-    res.json({pets: pets.rows})
-})
+router.get("/", async (req, res) => {
+  const pets = await db.query("SELECT * FROM pets");
+  res.json({ pets: pets.rows });
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await db.query("SELECT * FROM pets WHERE id = $1", [id]);
+  res.json({ pet: result.rows[0] });
+});
 
 module.exports = router;
