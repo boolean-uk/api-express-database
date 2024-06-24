fad41ecb99e115f42ffda814fe4e862f32a47118
Farshad <f.bagdeli13@gmail.com>
2024-06-24 19:22:23 +0200

    temporary changes

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -1,46 +1,46 @@
-const express = require('express')
-const router = express.Router()
-const { getAllBooks, createBook, getBookById, updateBook, deleteBook } = require('../dal/booksRepo.js')
+// const express = require('express')
+// const router = express.Router()
+// const { getAllBooks, createBook, getBookById, updateBook, deleteBook } = require('../dal/booksRepo.js')
 
-router.get('/', async (req, res) => {
-  const books = await getAllBooks()
-  res.status(200).json({
-    books
-  })
-})
+// router.get('/', async (req, res) => {
+//   const books = await getAllBooks()
+//   res.status(200).json({
+//     books
+//   })
+// })
 
-router.post('/', async (req, res) => {
-  const book = await createBook(req.body)
-  res.status(201).json({
-    book
-  })
-})
+// router.post('/', async (req, res) => {
+//   const book = await createBook(req.body)
+//   res.status(201).json({
+//     book
+//   })
+// })
 
-router.get('/:id', async (req, res) => {
-  const id = Number(req.params.id)
-  const book = await getBookById(id)
-  res.status(200).json({
-    book
-  })
-})
+// router.get('/:id', async (req, res) => {
+//   const id = Number(req.params.id)
+//   const book = await getBookById(id)
+//   res.status(200).json({
+//     book
+//   })
+// })
 
-router.put('/:id', async (req, res) => {
-  const id = Number(req.params.id)
-  const newBookInfo = req.body
+// router.put('/:id', async (req, res) => {
+//   const id = Number(req.params.id)
+//   const newBookInfo = req.body
 
-  const updatedBook = await updateBook(id, newBookInfo)
+//   const updatedBook = await updateBook(id, newBookInfo)
 
-  res.status(201).json({
-    book : updatedBook
-  })
-})
+//   res.status(201).json({
+//     book : updatedBook
+//   })
+// })
 
-router.delete('/:id', async (req, res) => {
-  const id = Number(req.params.id)
-  const deletedBook = await deleteBook(id)
-  res.status(201).json({
-    book : deletedBook
-  })
-})
+// router.delete('/:id', async (req, res) => {
+//   const id = Number(req.params.id)
+//   const deletedBook = await deleteBook(id)
+//   res.status(201).json({
+//     book : deletedBook
+//   })
+// })
 
-module.exports = router
+// module.exports = router
