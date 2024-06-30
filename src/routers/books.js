9403d3abc9143d7670c2005ac883b2b4a31113e4
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-06-30 17:25:08 +0100

    progress

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,9 +1,38 @@
+require('dotenv').config()
 const express = require('express')
-const router = express.Router()
-const db = require("../../db");
+const { Pool } = require('pg')
+
+//const db = require("../../db");
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const pool = new Pool({
+    host: PGHOST,
+    database: PGDATABASE,
+    username: PGUSER, 
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: {
+        require: true,
+    },
+});
+
+const router = express.Router();
 
 router.get('/', async (req, res) => {
+    const db = await pool.connect(); 
+    console.log('start');
+    const sqlQuery = 'SELECT * FROM books'; 
+    const result = await db.query(sqlQuery);
 
-})
+    res.json({ 
+        books: result.rows
+        
+    });
 
-module.exports = router
+    console.log(result.rows)
+    
+});
+
+module.exports = router;
+
