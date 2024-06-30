c7d930795573b2e414491ec7075c25014fb7f5af
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-06-30 19:47:43 +0100

    book pst done

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,15 +1,13 @@
-require('dotenv').config()
-const express = require('express')
-const { Pool } = require('pg')
-
-//const db = require("../../db");
+require('dotenv').config();
+const express = require('express');
+const { Pool } = require('pg');
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 const pool = new Pool({
     host: PGHOST,
     database: PGDATABASE,
-    username: PGUSER, 
+    user: PGUSER, 
     password: PGPASSWORD,
     port: 5432,
     ssl: {
@@ -21,18 +19,22 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     const db = await pool.connect(); 
-    console.log('start');
     const sqlQuery = 'SELECT * FROM books'; 
     const result = await db.query(sqlQuery);
 
     res.json({ 
         books: result.rows
-        
     });
+    db.release();
+});
 
-    console.log(result.rows)
-    
+router.post('/', async (req, res) => {
+    const { title, type, author, topic, publication_date, pages } = req.body;
+    const db = await pool.connect();
+    const sqlQuery = 'INSERT INTO books (title, type, author, topic, publication_date, pages) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+    const result = await db.query(sqlQuery, [title, type, author, topic, publication_date, pages]);
+    res.status(201).json({ book: result.rows[0] });
+    db.release();
 });
 
 module.exports = router;
-
