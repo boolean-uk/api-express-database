92d4fa837e8e9a370c4caabc593ce91e9619214f
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-06-30 20:18:42 +0100

    put done

--- src/routers/books.js
+++ src/routers/books.js
@@ -37,4 +37,40 @@ router.post('/', async (req, res) => {
     db.release();
 });
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const db = await pool.connect();
+    const sqlQuery = 'SELECT * FROM books WHERE id = $1';
+    const result = await db.query(sqlQuery, [id]);
+    
+    if (result.rows.length > 0) {
+        res.json({ book: result.rows[0] });
+    } else {
+        res.status(404).json({ error: 'Book not found' });
+    }
+
+    db.release();
+})
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, type, author, topic, publication_date, pages } = req.body;
+    const db = await pool.connect();
+    const sqlQuery = `
+        UPDATE books 
+        SET title = $1, type = $2, author = $3, topic = $4, publication_date = $5, pages = $6
+        WHERE id = $7
+        RETURNING *
+    `;
+    const result = await db.query(sqlQuery, [title, type, author, topic, publication_date, pages, id]);
+
+    if (result.rows.length > 0) {
+        res.json({ book: result.rows[0] });
+    } else {
+        res.status(404).json({ error: 'Book not found' });
+    }
+
+    db.release();
+});
+
 module.exports = router;
